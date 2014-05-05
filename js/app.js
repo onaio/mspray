eba651ced8fafc05eb112e483b2f4475c2acb5af
Matt Berg <mlberg@gmail.com>
2014-05-05 23:41:40 +0300

    MB: fixed counts hardcoded for now

--- js/app.js
+++ js/app.js
@@ -1,109 +1,119 @@
+
 var MYAPP = {};
 
 MYAPP.sprayData = null;
 MYAPP.hhData = null;
 
 MYAPP.day = 0;
-MYAPP.hh_sprayed = 0;
+//MYAPP.hh_sprayed = 0;
 MYAPP.hh_cnt = 0;
 MYAPP.spray_perc = 0;
 
-var geojson;
-var sprayLayer;  // buffer area
 
-var params = {};
-
+// get current day
 var myDay = location.search.split('day=')[1]
 
-
 if (myDay) {
-  MYAPP.day = parseInt(myDay.replace("\/", ""));
+    MYAPP.day = parseInt(myDay.replace("\/", ""));
 
 }
 
 
 function loadHH() {
-  d3.json("data/hh-chimbombo-1.geojson", function (json) {
-    var hhLayer = L.geoJson(json, {
-       pointToLayer: function (feature, latlng) {
-         return L.circleMarker(latlng, hhOptions);
-       }}).addTo(map);
-  });
-    
-   }
+    d3.json("data/hh-chimbombo-1.geojson", function (json) {
+        hhLayer = L.geoJson(json, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, hhOptions);
+            }
+        }).addTo(map);
+    });
+
+}
 
 function loadSprayPoints(day) {
 
-  d3.json("data/spray-day-"+day+".geojson", function (json) {
-    var hhLayer = L.geoJson(json, {
-       pointToLayer: function (feature, latlng) {
-         return L.circleMarker(latlng, sprayOptions);
-       }}).addTo(map);
-  });
-    
-   }
+    d3.json("data/spray-day-" + day + ".geojson", function (json) {
+        spraypointsLayer = L.geoJson(json, {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, sprayOptions);
+            }
+        }).addTo(map);
+    });
+
+}
 
 
 function loadSprayData() {
 
     if (MYAPP.sprayData === null) {
         d3.json("data/chimbombo-1-spray.geojson", function (json) {
-                sprayLayer = L.geoJson(json,  {
-      			style: getStyle,
-      			onEachFeature: onEachFeature
-			}).addTo(map);
+            sprayLayer = L.geoJson(json, {
+                style: getStyle,
+                onEachFeature: onEachFeature
+            }).addTo(map);
         });
 
     }
 };
 
 
+function loadDay() {
+    for (var i = 1; i <= MYAPP.day; i++) {
+        loadSprayPoints(i);
+    }
+    if (MYAPP.day === 1) {
+        var perc = 31;
+    }
+    switch(MYAPP.day) 
+    {
+        case 1: perc = 31;
+                break;
+        case 2: perc = 56;
+                break;
+        case 3: perc = 82;
+                break;
+        case 4: perc = 98;
+                break;
+        default: perc = 0;
+    }
 
-// Load 
-
-
+    drawCircle(perc);
+};
 
 // load household points
 
-var map = L.map('map').setView([-15.2164,28.2315], 15);
+var map = L.map('map').setView([-15.2164, 28.2315], 15);
 
-var legend = L.control({position: 'bottomright'});
+var legend = L.control({
+    position: 'bottomright'
+});
 L.control.locate().addTo(map);
 map.addLayer(new L.Google);
 
 
 L.mapbox.tileLayer('ona.q7gphkt9').addTo(map);
+buildLegend();
 
 loadSprayData();
 loadHH();
-buildLegend();
 loadDay();
 
-
-function loadDay() {
-  for (var i=1; i<=MYAPP.day; i++) {
-    loadSprayPoints(i);
-  }
-};
+console.log(MYAPP.hh_sprayed)
 
 
-
-drawCircle();
- 
-
-function drawCircle() {
+function drawCircle(percent) {
 
 
-Circles.create({
-    id:         'circles-1',
-    percentage: 32,
-    radius:     60,
-    width:      15,
-    number:     32,
-    text:       '%',
-    colors:     ['#AAAAAA', '#2ECC40'],
-    duration:   300
-});
+    Circles.create({
+        id: 'circles-1',
+        percentage: percent,
+        radius: 60,
+        width: 15,
+        number: percent,
+        text: '%',
+        colors: ['#AAAAAA', '#2ECC40'],
+        duration: 300
+    });
 }
 
 
@@ -137,16 +147,15 @@ info.onAdd = function (map) {
 
 
 info.update = function (props) {
-        if (props) {
-          var sprayed = sprayCount(MYAPP.day,props)
-        //var sprayed = props['day1-cnt'] + props['day2-cnt'] + props['day3-cnt'] + props['day4-cnt'];
-        var spray_percent = Math.round((sprayed / props['hh-cnt'])*100);
-        var hh_sprayed = spray_percent + '% (' + sprayed + '/' + props['hh-cnt'] +') HH sprayed';
-        }
-        this._div.innerHTML = (props ?
-        '<b>'+ hh_sprayed +'</b>'
-        : '');
-        
+    if (props) {
+        var sprayed = sprayCount(MYAPP.day, props)
+            //var sprayed = props['day1-cnt'] + props['day2-cnt'] + props['day3-cnt'] + props['day4-cnt'];
+        var spray_percent = Math.round((sprayed / props['hh-cnt']) * 100);
+        var hh_sprayed = spray_percent + '% (' + sprayed + '/' + props['hh-cnt'] + ') HH sprayed';
+    }
+    this._div.innerHTML = (props ?
+        '<b>' + hh_sprayed + '</b>' : '');
+
 };
 
 info.addTo(map);
@@ -173,46 +182,46 @@ function resetHighlight(e) {
 };
 
 
- function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
-  }
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
 
 
 function onEachFeature(feature, layer) {
-      layer.on({
+    layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
         click: zoomToFeature
-      });
+    });
 }
 
 
 
 function getStyle(feature) {
-      return {
-          weight: 2,
-          opacity: 0.1,
-          color: 'black',
-          fillOpacity: 0.4,
-          fillColor: getColor(feature.properties)
-      };
- 
+    return {
+        weight: 2,
+        opacity: 0.1,
+        color: 'black',
+        fillOpacity: 0.4,
+        fillColor: getColor(feature.properties)
+    };
+
 }
 
 
-function buildLegend () {
-    if(legend.getContainer() !== undefined) {
-        legend.removeFrom(map);    
+function buildLegend() {
+    if (legend.getContainer() !== undefined) {
+        legend.removeFrom(map);
     }
-    
+
     legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend'),
             labels = [];
-            labels.push('<i style="background:#2ECC40"></i> 100%');
-            labels.push('<i style="background:#FFDC00"></i> 66-99% ');
-            labels.push('<i style="background:#FF851B"></i> 33-66% ');
-            labels.push('<i style="background:#FF4136"></i> 1-33% ');
-            labels.push('<i style="background:#CCCCCC"></i> 0%');
+        labels.push('<i style="background:#2ECC40"></i> 100%');
+        labels.push('<i style="background:#FFDC00"></i> 66-99% ');
+        labels.push('<i style="background:#FF851B"></i> 33-66% ');
+        labels.push('<i style="background:#FF4136"></i> 1-33% ');
+        labels.push('<i style="background:#CCCCCC"></i> 0%');
         div.innerHTML = labels.join('<br><br>');
         return div;
     };
@@ -221,49 +230,41 @@ function buildLegend () {
 };
 
 function getColor(d) {
-    	
-      MYAPP.hh_sprayed = MYAPP.hh_sprayed + sprayCount(MYAPP.day,d)
-      MYAPP.hh_cnt = MYAPP.hh_cnt + d['hh-cnt']
-      //console.log(MYAPP.spray_perc = MYAPP.hh_sprayed / MYAPP.hh_cnt);
-     
-   
 
-      var spray_rate = sprayRate(MYAPP.day,d)
-    
-        return spray_rate === 1  ? '#2ECC40' :
-        	   spray_rate > 0.66 ?	'#FFDC00' : 
-               spray_rate > 0.33 ? '#FF851B' :
-               spray_rate > 0 ? '#FF4136' :
-                '#ccc';
-          };
+    MYAPP.hh_sprayed = MYAPP.hh_sprayed + sprayCount(MYAPP.day, d)
+
+    MYAPP.hh_cnt = MYAPP.hh_cnt + d['hh-cnt']
+    MYAPP.spray_perc = MYAPP.hh_sprayed / MYAPP.hh_cnt;
 
 
 
 
-function sprayRate(day,d) {
- 
-	return day === 1 ? d['day1-cnt'] / d['hh-cnt'] :
-	       day === 2 ? (d['day1-cnt']+ d['day2-cnt']) / d['hh-cnt'] :
-	       day === 3 ? (d['day1-cnt']+ d['day2-cnt']+ d['day3-cnt']) / d['hh-cnt'] :
-	       day === 4 ? (d['day1-cnt']+ d['day2-cnt']+ d['day3-cnt']+ d['day4-cnt']) / d['hh-cnt'] :
-		     0;
-};
+    var spray_rate = sprayRate(MYAPP.day, d)
 
-
-function sprayCount(day,d) {
-  
-  return day === 1 ? d['day1-cnt'] :
-         day === 2 ? (d['day1-cnt']+ d['day2-cnt']):
-         day === 3 ? (d['day1-cnt']+ d['day2-cnt']+ d['day3-cnt']) :
-         day === 4 ? (d['day1-cnt']+ d['day2-cnt']+ d['day3-cnt']+ d['day4-cnt']) :
-       0;
+    return spray_rate === 1 ? '#2ECC40' :
+        spray_rate > 0.66 ? '#FFDC00' :
+        spray_rate > 0.33 ? '#FF851B' :
+        spray_rate > 0 ? '#FF4136' :
+        '#ccc';
 };
 
 
 
+function sprayRate(day, d) {
+
+    return day === 1 ? d['day1-cnt'] / d['hh-cnt'] :
+        day === 2 ? (d['day1-cnt'] + d['day2-cnt']) / d['hh-cnt'] :
+        day === 3 ? (d['day1-cnt'] + d['day2-cnt'] + d['day3-cnt']) / d['hh-cnt'] :
+        day === 4 ? (d['day1-cnt'] + d['day2-cnt'] + d['day3-cnt'] + d['day4-cnt']) / d['hh-cnt'] :
+        0;
+};
 
 
+function sprayCount(day, d) {
 
-
-
-
+    return day === 1 ? d['day1-cnt'] :
+        day === 2 ? (d['day1-cnt'] + d['day2-cnt']) :
+        day === 3 ? (d['day1-cnt'] + d['day2-cnt'] + d['day3-cnt']) :
+        day === 4 ? (d['day1-cnt'] + d['day2-cnt'] + d['day3-cnt'] + d['day4-cnt']) :
+        0;
+};
