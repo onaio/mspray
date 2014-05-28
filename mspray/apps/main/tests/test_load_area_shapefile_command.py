70665971c3ff5add9b13cd0696938e43c07d6832
Ukang'a Dickson <ukanga@gmail.com>
2014-05-28 22:17:16 +0300

    UK: load_area_shapefile command

---
+++ mspray/apps/main/tests/test_load_area_shapefile_command.py
@@ -0,0 +1,16 @@
+from django.core.management import call_command
+
+from mspray.apps.main.models.target_area import TargetArea
+from mspray.apps.main.tests.test_base import TestBase
+
+
+class TestUtils(TestBase):
+    def setUp(self):
+        super(TestUtils, self).setUp()
+
+    def test_import_shapefile(self):
+        count = TargetArea.objects.count()
+
+        call_command('load_area_shapefile', self.area_shp)
+
+        self.assertTrue(count + 1 == TargetArea.objects.count())
