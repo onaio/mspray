544e9a124a4ad24c5d05b06d9dadc72a547696e4
Ukang'a Dickson <ukanga@gmail.com>
2014-05-29 10:33:14 +0300

    UK: add load_household_shapefile command

---
+++ mspray/apps/main/management/commands/load_household_shapefile.py
@@ -0,0 +1,20 @@
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import gettext as _
+
+from mspray.apps.main import utils
+
+
+class Command(BaseCommand):
+    args = '<path to shapefile>'
+    help = _(u"Load an area shapefile")
+
+    def handle(self, *args, **options):
+        for path in args:
+            try:
+                path = os.path.abspath(path)
+            except Exception as e:
+                raise CommandError(_(u"Error: %(msg)s" % {"msg": e}))
+            else:
+                utils.load_household_layer_mapping(path, verbose=True)
