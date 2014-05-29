5efce2b177a901aa9fff4dd6e66fba705d45a750
Ukang'a Dickson <ukanga@gmail.com>
2014-05-29 10:36:57 +0300

    UK: add Household model to admin.py

--- mspray/apps/main/admin.py
+++ mspray/apps/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib.gis import admin
+
+from mspray.apps.main.models.household import Household
 from mspray.apps.main.models.target_area import TargetArea
 
+admin.site.register(Household, admin.GeoModelAdmin)
 admin.site.register(TargetArea, admin.GeoModelAdmin)
