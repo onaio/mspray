153bd38056594c4c09e8441c608c0599328d8b75
Ukang'a Dickson <ukanga@gmail.com>
2014-05-29 14:35:34 +0300

    UK: add restframework, targetarea geojson endpoint, touches on #7

--- mspray/settings.py
+++ mspray/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'django_nose',
+    'rest_framework',
     'south',
     'mspray.apps.main'
 )
--- mspray/urls.py
+++ mspray/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from rest_framework import routers
 from django.contrib import admin
 admin.autodiscover()
+
+from mspray.apps.main.views import target_area
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'targetarea', target_area.TargetAreaViewSet)
 
 urlpatterns = patterns(
     '',
@@ -9,5 +15,6 @@ urlpatterns = patterns(
     # url(r'^$', 'mspray.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
