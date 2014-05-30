df388db63efb6a0dc3c27c003fed78ce53da75f8
Ukang'a Dickson <ukanga@gmail.com>
2014-05-30 11:27:25 +0300

    UK: add django-filter, filter spray days endpoint, closes #12

--- mspray/apps/main/views/sprayday.py
+++ mspray/apps/main/views/sprayday.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework import viewsets
 
 from mspray.apps.main.models.spray_day import SprayDay
@@ -7,3 +8,5 @@ from mspray.apps.main.serializers.sprayday import SprayDaySerializer
 class SprayDayViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SprayDay.objects.all()
     serializer_class = SprayDaySerializer
+    filter_backends = (filters.DjangoFilterBackend, )
+    filter_fields = ('day',)
