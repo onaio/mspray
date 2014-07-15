f98e0a1eccbc8df72b2a1e5545b09fd3ef22fdaf
William Mucheru <willyk99@gmail.com>
2014-07-15 12:28:43 +0300

    WM: Load target areas from API in ng

--- ui/js/custom.js
+++ ui/js/custom.js
@@ -2,20 +2,29 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('TargetCtrl', ['$scope', '$http', function($scope, $http){
     
-    $scope.targetURI = 'http://api.mspray.onalabs.org/targetareas.json';
+    $scope.targetURI = 'http://api.mspray.onalabs.org/districts.json';
     $scope.filterText = '';
     
-    $scope.targetData = [
-        {
-            'targetId': '1',
-            'ranks': '155',
-            'houses': 't41',
-        },
-        {
-            'targetId': '2',
-            'ranks': '193',
-            'houses': 't97',
-        }
-    ];
+    // $scope.targetData = [
+        // {
+            // 'targetid': '1',
+            // 'ranks': '155',
+            // 'houses': 't41',
+        // },
+        // {
+            // 'targetid': '2',
+            // 'ranks': '193',
+            // 'houses': 't97',
+        // }
+    // ];
+    $scope.targetData={}; 
     
+    var getTargetAreas = $http.get($scope.targetURI + '?district=Serenje');
+
+    getTargetAreas.success(function(data, status, headers, config) {
+        $scope.targetData = data;
+    });
+    getTargetAreas.error(function(data, status, headers, config) {
+        console.log('Could not retrieve data.');
+    });
 }]);
