4c902e42cf16de3afe3476ee8230344c102771b8
Ukang'a Dickson <ukanga@gmail.com>
2014-08-05 12:40:28 +0300

    UK: test household buffer areas

--- mspray/apps/main/tests/views/test_household.py
+++ mspray/apps/main/tests/views/test_household.py
@@ -9,16 +9,11 @@ from mspray.apps.main.views.household import HouseholdViewSet
 class TestHouseholdViewSet(TestBase):
     def setUp(self):
         super(TestHouseholdViewSet, self).setUp()
-        fixtures_dir = os.path.join(
-            os.path.dirname(__file__), '../', 'fixtures'
-        )
-        call_command('loaddata',
-                     os.path.join(fixtures_dir, 'target_areas.json'))
-        call_command('loaddata',
-                     os.path.join(fixtures_dir, 'households.json'))
+        self.fixtures = ['target_areas', 'households']
         self.view = HouseholdViewSet.as_view({'get': 'list'})
 
     def test_household_list_view(self):
+        self._loaddata_fixtures(self.fixtures)
         request = self.factory.get('/')
         response = self.view(request)
         self.assertEqual(response.status_code, 200)
@@ -26,6 +21,7 @@ class TestHouseholdViewSet(TestBase):
         self.assertEqual(len(response.data['features']), 100)
 
     def test_household_list_view_filter_by_target_area(self):
+        self._loaddata_fixtures(self.fixtures)
         data = {'target_area': 277}
         request = self.factory.get('/', data)
         response = self.view(request)
@@ -34,13 +30,33 @@ class TestHouseholdViewSet(TestBase):
         self.assertEqual(len(response.data['features']), 1)
 
     def test_household_list_view_filter_by_nonexistent_target_area(self):
+        self._loaddata_fixtures(self.fixtures)
         data = {'target_area': 21234}
         request = self.factory.get('/', data)
         response = self.view(request)
         self.assertEqual(response.status_code, 404)
 
     def test_household_list_view_filter_by_invalid_target_area(self):
+        self._loaddata_fixtures(self.fixtures)
         data = {'target_area': "dummy"}
         request = self.factory.get('/', data)
         response = self.view(request)
         self.assertEqual(response.status_code, 400)
+
+    def test_household_list_view_get_buffers(self):
+        self.fixtures = [
+            '848_households', '848_target_area', '848_spraypoints']
+        self._loaddata_fixtures(self.fixtures)
+        data = {'target_area': 848}
+        request = self.factory.get('/', data)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('features', response.data)
+        self.assertEqual(len(response.data['features']), 377)
+
+        data = {'target_area': 848, 'buffer': 'true'}
+        request = self.factory.get('/', data)
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 183)
+        self.assertEqual(response.data[0]['type'], 'Polygon')
