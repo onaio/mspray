4ffce38af0f25b7ac7fdfa81dc2e40ec4dfb9cdf
Ukang'a Dickson <ukanga@gmail.com>
2014-08-05 11:28:31 +0300

    test ordering

--- mspray/apps/main/tests/views/test_sprayday.py
+++ mspray/apps/main/tests/views/test_sprayday.py
@@ -14,6 +14,24 @@ class TestSprayDayViewSet(TestBase):
         self.assertEqual(response.data['features'][0]['properties']['day'],
                          1)
 
+    def test_spraydays_list_view_ordering(self):
+        self._load_spraydays_shapefile()
+        view = SprayDayViewSet.as_view({'get': 'list'})
+        request = self.factory.get('/')
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('features', response.data)
+        self.assertEqual(response.data['features'][0]['properties']['day'],
+                         1)
+        # reverse order
+        data = {'ordering': '-day'}
+        request = self.factory.get('/', data)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('features', response.data)
+        self.assertEqual(response.data['features'][0]['properties']['day'],
+                         4)
+
     def test_recieve_json_post(self):
         count = SprayDay.objects.count()
         data = '{"_id": 2, "name": "submission",'\
