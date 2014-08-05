9e7b617efa5f90bfee2326d80f747003e38b609d
Ukang'a Dickson <ukanga@gmail.com>
2014-08-05 18:44:59 +0300

    UK: add create_household_buffers management command

---
+++ mspray/apps/main/tests/management/commands/test_create_household_buffers.py
@@ -0,0 +1,13 @@
+from django.core.management import call_command
+from mspray.apps.main.models.households_buffer import HouseholdsBuffer
+
+from mspray.apps.main.tests.test_base import TestBase
+
+
+class TestCreateHouseholdBuffers(TestBase):
+    def test_create_household_buffers(self):
+        count = HouseholdsBuffer.objects.count()
+        self._loaddata_fixtures(['848_target_area', '848_households'])
+        call_command('create_household_buffers')
+
+        self.assertEqual(HouseholdsBuffer.objects.count(), count + 183)
