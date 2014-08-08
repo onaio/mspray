f3194b773314dce1ef59ca65aca192fc7f7d827b
Ukang'a Dickson <ukanga@gmail.com>
2014-08-08 19:14:51 +0300

    UK/ME/WM: search link loads map

--- ui/js/app.js
+++ ui/js/app.js
@@ -95,7 +95,7 @@ var App = {
                         num_targets = list_data.num_target_areas,
 
                         // dist_data = '<li><a href="#!'+ dist_name +'">'+ dist_name +'</a></li>';
-                        dist_data = '<li><a href="ta.html#!' + dist_name +'">'+ dist_name +'</a></li>';
+                        dist_data = '<li><a href="/#!' + dist_name +'">'+ dist_name +'</a></li>';
 
                     d_list.append(dist_data);
                 }
@@ -172,11 +172,11 @@ var App = {
                     agg_visited_other += parseInt(visited_other);
                     agg_not_visited += parseInt(not_visited);
 
-                    target_list_content += '<li><a href="#!'+ district_name + "/" +
+                    target_list_content += '<li><a href="/ta.html#!'+ district_name + "/" +
                             target_id + '">'+ target_id +'</a></li>';
 
                     target_table_content += '<tr>'+
-                            '<th><a href="ta.html#!'+ district_name + "/" + target_id + '">'+ target_id +'</a></th>' +
+                            '<th><a href="/ta.html#!'+ district_name + "/" + target_id + '">'+ target_id +'</a></th>' +
                             '<td>' + structures + '</td>' +
                             '<td>' + visited_total + ' (' + App.calcaulatePercentage(visited_total, structures) + ')</td>' +
                             '<td>' + visited_sprayed + ' (' +  App.calcaulatePercentage(visited_sprayed, structures) + ')</td>' +
