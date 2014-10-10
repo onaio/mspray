91bd961b5810385f955780e4f8133a77782a36c0
Ukang'a Dickson <ukanga@gmail.com>
2014-10-10 11:10:14 +0300

    UK: options for locate

--- ui/js/app.js
+++ ui/js/app.js
@@ -603,7 +603,15 @@ var App = {
         }, {
             'buffershouseholds': bufferHouseholdsLayer
         }).addTo(App.map);
-        L.control.locate({locateOptions: {enableHighAccuracy: true, maximumAge: 0}}).addTo(App.map);
+        var locateOptions = {
+            remainActive: true,
+            locateOptions: {
+                enableHighAccuracy: true,
+                maximumAge: 0,
+                watch: true
+            }
+        };
+        L.control.locate().addTo(App.map);
         L.control.scale({
             position: 'bottomright'
         }).addTo(App.map);
