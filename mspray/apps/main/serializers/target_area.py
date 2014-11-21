a74e67909f88119576ff18f6dfc74e1ad2024ad7
ekisa <mark.ekisa@gmail.com>
2014-11-21 17:36:33 +0300

    ME: added 'visited_not_sprayed' property and changed 'get_visited_other' functionality

--- mspray/apps/main/serializers/target_area.py
+++ mspray/apps/main/serializers/target_area.py
@@ -1,4 +1,5 @@
 from django.core.cache import cache
+from django.db.models import Q
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from rest_framework_gis.fields import GeometryField
@@ -25,6 +26,7 @@ def cached_queryset_count(key, queryset, query=None):
 
 
 class TargetAreaMixin(object):
+
     def get_visited_total(self, obj):
         if obj:
             key = "%s_visited_total" % obj.pk
@@ -38,6 +40,15 @@ class TargetAreaMixin(object):
             queryset = SprayDay.objects.filter(
                 geom__coveredby=obj.geom
             ).filter(data__contains='"sprayed/was_sprayed":"yes"')
+
+            return cached_queryset_count(key, queryset)
+
+    def get_visited_not_sprayed(self, obj):
+        if obj:
+            key = "%s_visited_not_sprayed" % obj.pk
+            queryset = SprayDay.objects.filter(
+                geom__coveredby=obj.geom
+            ).filter(data__contains='"sprayed/was_sprayed":"no"')
 
             return cached_queryset_count(key, queryset)
 
@@ -55,7 +66,12 @@ class TargetAreaMixin(object):
             key = "%s_visited_other" % obj.pk
             queryset = SprayDay.objects.filter(
                 geom__coveredby=obj.geom
-            ).filter(data__contains='"unsprayed/reason":"Other"')
+            ).filter(
+                Q(data__contains='"unsprayed/reason":"Other"') |
+                Q(data__contains='"unsprayed/reason":"Sick"') |
+                Q(data__contains='"unsprayed/reason":"Funeral"') |
+                Q(data__contains='"unsprayed/reason":"Locked"') |
+                Q(data__contains='"unsprayed/reason":"No One Home/Missing"'))
 
             return cached_queryset_count(key, queryset)
 
@@ -79,6 +95,8 @@ class TargetAreaSerializer(TargetAreaMixin, serializers.ModelSerializer):
     structures = serializers.Field(source='houses')
     visited_total = serializers.SerializerMethodField('get_visited_total')
     visited_sprayed = serializers.SerializerMethodField('get_visited_sprayed')
+    visited_not_sprayed = serializers.SerializerMethodField(
+        'get_visited_not_sprayed')
     visited_refused = serializers.SerializerMethodField('get_visited_refused')
     visited_other = serializers.SerializerMethodField('get_visited_other')
     not_visited = serializers.SerializerMethodField('get_not_visited')
@@ -86,7 +104,8 @@ class TargetAreaSerializer(TargetAreaMixin, serializers.ModelSerializer):
 
     class Meta:
         fields = ('targetid', 'structures', 'visited_total', 'visited_sprayed',
-                  'visited_refused', 'visited_other', 'not_visited', 'bounds')
+                  'visited_not_sprayed', 'visited_refused', 'visited_other',
+                  'not_visited', 'bounds')
         model = TargetArea
 
 
@@ -95,6 +114,8 @@ class GeoTargetAreaSerializer(TargetAreaMixin, GeoFeatureModelSerializer):
     structures = serializers.Field(source='houses')
     visited_total = serializers.SerializerMethodField('get_visited_total')
     visited_sprayed = serializers.SerializerMethodField('get_visited_sprayed')
+    visited_not_sprayed = serializers.SerializerMethodField(
+        'get_visited_not_sprayed')
     visited_refused = serializers.SerializerMethodField('get_visited_refused')
     visited_other = serializers.SerializerMethodField('get_visited_other')
     not_visited = serializers.SerializerMethodField('get_not_visited')
@@ -102,6 +123,7 @@ class GeoTargetAreaSerializer(TargetAreaMixin, GeoFeatureModelSerializer):
 
     class Meta:
         fields = ('targetid', 'structures', 'visited_total', 'visited_sprayed',
-                  'visited_refused', 'visited_other', 'not_visited')
+                  'visited_not_sprayed', 'visited_refused', 'visited_other',
+                  'not_visited')
         model = TargetArea
         geo_field = 'geom'
