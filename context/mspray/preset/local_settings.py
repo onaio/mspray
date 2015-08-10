c3afc511abf7492dcbc3cb3d9ef1e67afd4b2084
Ukang'a Dickson <ukanga@gmail.com>
2015-08-10 12:55:44 +0300

    local_settings.py

---
+++ context/mspray/preset/local_settings.py
@@ -0,0 +1,28 @@
+from mspray.settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'REPLACE_DB_NAME',
+        'USER': 'REPLACE_DB_USER',
+        'PASSWORD': 'REPLACE_DB_PASSWORD',
+        'HOST': 'REPLACE_DB_HOST',
+    }
+}
+
+NOSE_ARGS = ['--stop']
+NOSE_PLUGINS = ['mspray.libs.utils.nose_plugins.SilenceSouth']
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'mspray.onalabs.org',
+    'api.mspray.onalabs.org',
+)
