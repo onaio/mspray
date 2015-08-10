346b041e687acb305c2238e6aa2ca1f14720e307
Ukang'a Dickson <ukanga@gmail.com>
2015-08-10 11:18:19 +0300

    district view

--- mspray/apps/madagascar/urls.py
+++ mspray/apps/madagascar/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index')
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<slug>\w+)$', views.DistrictView.as_view(),
+        name='district')
 ]
--- mspray/apps/madagascar/views.py
+++ mspray/apps/madagascar/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.views.generic import DetailView
 
 from mspray.apps.main.models import District
+from mspray.apps.main.models import TargetArea
 from mspray.apps.main.serializers.target_area import TargetAreaSerializer
 
 
@@ -14,3 +16,20 @@ class IndexView(View):
         context = {'district_list': serializer.data}
 
         return render(request, 'madagascar/index.html', context)
+
+
+class DistrictView(DetailView):
+    template_name = 'madagascar/index.html'
+    model = District
+    slug_field = 'district_name'
+
+    def get_context_data(self, **kwargs):
+        context = super(DistrictView, self).get_context_data(**kwargs)
+        qs = TargetArea.objects.filter(
+            district_name=kwargs['object'].district_name
+        )
+        serializer = TargetAreaSerializer(qs, many=True,
+                                          context={'request': self.request})
+        context['district_list'] = serializer.data
+
+        return context
