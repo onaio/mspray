834eef9ffcc2976ac666870fce3de216de9ccbf3
ivermac <mark.ekisa@gmail.com>
2015-08-14 17:53:18 +0300

    ME: added templates path

--- mspray/settings.py
+++ mspray/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'mspray/apps/main/templates'),
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
