38cd0923204a216f5ad1933fe7533b05d5f3c5db
ivermac <mark.ekisa@gmail.com>
2015-08-17 10:44:27 +0300

    ME: added urls

--- mspray/urls.py
+++ mspray/urls.py
@@ -1,13 +1,12 @@
+from mspray.apps.main.views import (
+    target_area, household, household_buffer, sprayday, indicators, districts
+)
+
 from django.conf import settings
 from django.conf.urls import patterns, include, url, static
 from rest_framework import routers
 from django.contrib import admin
 admin.autodiscover()
-
-from mspray.apps.main.views import (
-    target_area, household, household_buffer, sprayday, indicators, districts,
-    performance
-)
 
 router = routers.DefaultRouter(trailing_slash=False)
 
@@ -30,6 +29,10 @@ urlpatterns = patterns(
     url(r'^madagascar/', include('mspray.apps.madagascar.urls',
                                  namespace='madagascar')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^performance/',
+    url(r'^performance/district',
         'mspray.apps.main.views.performance.district'),
+    url(r'^performance/team-leader',
+        'mspray.apps.main.views.performance.team_leader'),
+    url(r'^definitions-and-conditions',
+        'mspray.apps.main.views.performance.definitions_and_conditions'),
 ) + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
