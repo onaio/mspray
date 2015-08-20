5690cbebfe3bd4ccd0e75500f2f3aad4f3286f36
Ukang'a Dickson <ukanga@gmail.com>
2015-08-20 16:27:33 +0300

    load team leaders from csv file command

---
+++ mspray/apps/main/management/commands/load_team_leaders.py
@@ -0,0 +1,33 @@
+import codecs
+import csv
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import gettext as _
+
+from mspray.apps.main.models import TeamLeader
+
+
+class Command(BaseCommand):
+    args = '<path to team leaders csv with columns code|name>'
+    help = _('Load team leaders')
+
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            raise CommandError(_('Missing locations csv file path'))
+        for path in args:
+            try:
+                path = os.path.abspath(path)
+            except Exception as e:
+                raise CommandError(_('Error: %(msg)s' % {"msg": e}))
+            else:
+                with codecs.open(path, encoding='utf-8') as f:
+                    csv_reader = csv.DictReader(f)
+                    for row in csv_reader:
+                        team_leader, created = \
+                            TeamLeader.objects.get_or_create(
+                                code=int(row['code']),
+                                name=row['name']
+                            )
+                        if created:
+                            print(row)
