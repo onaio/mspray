b7f2c12409cfe3a9d33a9d6b8ce535ded0db7747
Ukang'a Dickson <ukanga@gmail.com>
2015-08-31 08:55:36 +0300

    locations model

---
+++ mspray/apps/main/models/location.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.db import models
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255, db_index=1)
+    geom = models.MultiPolygonField(srid=4326)
+    code = models.CharField(max_length=10, db_index=1)
+    level = models.CharField(db_index=1, max_length=50)
+    parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
+    objects = models.GeoManager()
+
+    class Meta:
+        app_label = 'main'
+
+    def __str__(self):
+        return self.name
