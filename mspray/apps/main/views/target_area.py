f8616e5e2f8b7edbfd063d513d8388213c456583
Ukang'a Dickson <ukanga@gmail.com>
2015-09-08 08:25:16 +0300

    use Location model for viewset

--- mspray/apps/main/views/target_area.py
+++ mspray/apps/main/views/target_area.py
@@ -1,13 +1,13 @@
-from django.shortcuts import get_list_or_404
 from rest_framework import viewsets
+from rest_framework import mixins
 
-from mspray.apps.main.models.target_area import TargetArea
+from mspray.apps.main.models import Location
 from mspray.apps.main.serializers.target_area import (
     TargetAreaSerializer, GeoTargetAreaSerializer)
 
 
-class TargetAreaViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = TargetArea.objects.filter(targeted=TargetArea.TARGETED_VALUE)
+class TargetAreaViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Location.objects.filter()
     serializer_class = TargetAreaSerializer
 
     def get_serializer_class(self):
@@ -15,12 +15,3 @@ class TargetAreaViewSet(viewsets.ReadOnlyModelViewSet):
             return GeoTargetAreaSerializer
 
         return super(TargetAreaViewSet, self).get_serializer_class()
-
-    def filter_queryset(self, queryset):
-        targetid = self.request.QUERY_PARAMS.get('target_area')
-
-        if targetid:
-            queryset = get_list_or_404(TargetArea, targetid=targetid,
-                                       targeted=TargetArea.TARGETED_VALUE)
-
-        return super(TargetAreaViewSet, self).filter_queryset(queryset)
