17a23c8730a7118c23c09bf6534e4ef9059a92ac
Ukang'a Dickson <ukanga@gmail.com>
2015-09-16 17:19:40 +0300

    initial osm handling utilities

---
+++ mspray/apps/main/osm.py
@@ -0,0 +1,41 @@
+from django.contrib.gis.geos import LineString
+from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Polygon
+from io import StringIO
+from lxml import etree
+
+
+def _get_xml_obj(xml):
+
+    xml = xml.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
+
+    return etree.parse(StringIO(xml))
+
+
+def _get_node(ref, root):
+    point = None
+    nodes = root.xpath('//node[@id="{}"]'.format(ref))
+    if nodes:
+        node = nodes[0]
+        x, y = float(node.get('lon')), float(node.get('lat'))
+        point = Point(x, y)
+
+    return point
+
+
+def parse_osm_ways(osm_xml):
+    """Converts an OSM XMl to a list of GEOSGeometry objects """
+    items = []
+
+    root = _get_xml_obj(osm_xml)
+
+    for way in root.findall('way'):
+        points = []
+        for nd in way.findall('nd'):
+            points.append(_get_node(nd.get('ref'), root))
+        try:
+            items.append(Polygon(points))
+        except:
+            items.append(LineString(points))
+
+    return items
