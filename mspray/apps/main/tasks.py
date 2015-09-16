4ba9228d554177e4a37684645de3bdef97ab66ba
Ukang'a Dickson <ukanga@gmail.com>
2015-09-16 20:45:04 +0300

    initial work on linking osm data to a spraypoint as a task

---
+++ mspray/apps/main/tasks.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+
+from mspray.apps.main.models import Location
+from mspray.apps.main.models import SprayDay
+from mspray.apps.main.ona import fetch_osm_xml
+from mspray.apps.main.osm import parse_osm_ways
+
+
+def link_spraypoint_with_osm(pk):
+    try:
+        sp = SprayDay.objects.get(pk=pk)
+    except SprayDay.DoesNotExist:
+        pass
+    else:
+        osm_xml = fetch_osm_xml(sp.data)
+        if osm_xml is not None:
+            ways = parse_osm_ways(osm_xml)
+            if len(ways):
+                way = ways[0]
+                locations = Location.objects.filter(
+                    geom__covers=way.centroid,
+                    level=settings.MSPRAY_TA_LEVEL
+                )
+                if locations.count():
+                    location = locations.first()
+                    sp.geom = way.centroid
+                    sp.location = location
+                    sp.save()
---
+++ mspray/apps/main/tests/test_tasks.py
@@ -0,0 +1,37 @@
+from httmock import urlmatch, HTTMock
+from django.test import TestCase
+
+from mspray.apps.main.models import SprayDay
+from mspray.apps.main.tasks import link_spraypoint_with_osm
+from mspray.apps.main.utils import add_spray_data
+
+OSMXML = """<?xml version='1.0' encoding='UTF-8' ?><osm version="0.6" generator="OpenMapKit 0.7" user="theoutpost"><node id="-1943" lat="-11.202901601" lon="28.883830387" /><node id="-1946" lat="-11.202926082" lon="28.883944473" /><node id="-1945" lat="-11.202845645" lon="28.88396943" /><node id="-1944" lat="-11.202821164" lon="28.883858908" /><node id="-1943" lat="-11.202901601" lon="28.883830387" /><way id="-1942" action="modify"><nd ref="-1943" /><nd ref="-1946" /><nd ref="-1945" /><nd ref="-1944" /><nd ref="-1943" /><tag k="Shape_Area" v="0.00000000969" /><tag k="district_1" v="Mansa" /><tag k="manual_c_1" v="Targeted" /><tag k="OBJECTID" v="79621" /><tag k="rank_1" v="300.000000" /><tag k="province_1" v="Luapula" /><tag k="Shape_Leng" v="0.00039944548" /><tag k="psa_id_1" v="300 / 450" /><tag k="y" v="-11.20287380280" /><tag k="x3" v="28.88390064920" /><tag k="structur_1" v="450.000000" /><tag k="id" v="300 / 450_Mansa" /><tag k="spray_status" v="yes" /></way></osm>"""  # noqa
+SUBMISSION_DATA = {
+    "today": "2015-09-16",
+    "osm_building": "OSMWay-1760.osm",
+    "_id": 3563261,
+    "_attachments": [{
+        "mimetype": "text/xml",
+        "download_url": "/api/v1/files/583377?filename=osm_experiments/attachments/OSMWay-1942.osm",  # noqa
+        "filename": "osm_experiments/attachments/OSMWay-1942.osm",
+        "instance": 3542171,
+        "id": 583377,
+        "xform": 79639
+    }],
+    "meta/instanceID": "uuid:da51a5c9-e87d-49df-9559-43f670f2079b"
+}
+
+
+@urlmatch(netloc=r'(.*\.)?ona\.io$')
+def onaio_mock(url, request):
+    return OSMXML.strip()
+
+
+class TestTasks(TestCase):
+    def test_link_spraypoint_with_osm(self):
+        sp = add_spray_data(SUBMISSION_DATA)
+
+        with HTTMock(onaio_mock):
+            link_spraypoint_with_osm(sp.pk)
+            sp = SprayDay.objects.get(pk=sp.pk)
+            self.assertTrue(sp.location is not None)
