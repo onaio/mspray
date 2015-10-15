689a2ae5a20f66a1f5829410ef19050802d553f8
Ukang'a Dickson <ukanga@gmail.com>
2015-10-15 19:13:23 +0300

    fix spray ta

---
+++ mspray/apps/main/management/commands/fix_spray_target_area.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+from mspray.apps.main.models import SprayDay
+from mspray.apps.main.models import Location
+
+
+class Command(BaseCommand):
+    help = _('Set location based on target area field in data')
+
+    def handle(self, *args, **options):
+        count = 0
+        points = SprayDay.objects.filter(location=None)
+        final_count = points.count()
+        for sprayday in points.iterator():
+            ta = sprayday.data.get('target_area')
+            osm_structure = sprayday.data.get('osmstructure')
+            try:
+                location = Location.objects.get(name=ta.replace('_', ' / '))
+                if not osm_structure:
+                    sprayday.location = location
+                    sprayday.save()
+                    count += 1
+            except Location.DoesNotExist:
+                pass
+
+        self.stdout.write(
+            "{} points of {} linked to location.".format(
+                count, final_count
+            )
+        )
