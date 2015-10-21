b9f7ce2d9d0b5be5fe155866a7484b9c9f177ce1
Ukang'a Dickson <ukanga@gmail.com>
2015-10-21 12:23:58 +0300

    handle new structure

--- mspray/apps/main/tasks.py
+++ mspray/apps/main/tasks.py
@@ -7,12 +7,34 @@ from mspray.apps.main.models import SprayDay
 from mspray.apps.main.ona import fetch_osm_xml
 from mspray.apps.main.osm import parse_osm_ways
 from mspray.apps.main.osm import parse_osm_nodes
+from mspray.apps.main.models.spray_day import STRUCTURE_GPS_FIELD
+from mspray.apps.main.models.spray_day import NON_STRUCTURE_GPS_FIELD
 from mspray.celery import app
 
 HAS_UNIQUE_FIELD = getattr(settings, 'MSPRAY_UNIQUE_FIELD', None)
+STRUCTURE_GPS_FIELD = getattr(settings,
+                              'MSPRAY_STRUCTURE_GPS_FIELD',
+                              STRUCTURE_GPS_FIELD)
 
 
-def get_location_from_data(data):
+def get_new_structure_location(data, geom):
+    from mspray.apps.main.utils import geojson_from_gps_string
+    gps_field = data.get(STRUCTURE_GPS_FIELD,
+                         data.get(NON_STRUCTURE_GPS_FIELD))
+    geom = geojson_from_gps_string(gps_field) \
+        if gps_field is not None else geom
+    if geom is not None:
+        locations = Location.objects.filter(
+            geom__contains=geom,
+            level=settings.MSPRAY_TA_LEVEL
+        )
+        if locations:
+            location = locations[0]
+
+    return location, geom
+
+
+def get_location_from_data(data, geom):
     district = data.get('district')
     target_area = data.get('target_area')
     location = None
@@ -76,7 +98,11 @@ def link_spraypoint_with_osm(pk):
     else:
         location, geom, is_node = get_location_from_osm(sp.data)
         if location is None:
-            location = get_location_from_data(sp.data)
+            location, geom = get_new_structure_location(sp.data, geom)
+            if location is None:
+                location = get_location_from_data(sp.data)
+            else:
+                is_node = True
         set_spraypoint_location(sp, location, geom, is_node)
 
         return sp.pk
