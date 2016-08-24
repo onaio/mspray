23d223abe5d43ea9bf697eb545176ea10d8c98b7
ivermac <mark.ekisa@gmail.com>
2016-08-24 11:20:04 +0300

    ME: added templatetag with spray effectiveness functionality and percentage calculation

---
+++ mspray/apps/main/templatetags/progress_extras.py
@@ -0,0 +1,40 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def sprayed_effectively_color(value):
+    # ignore last character because it's a percentage
+    if isinstance(value, str):
+        value = value[:-1]
+    try:
+        if not isinstance(value, (int, float, complex)):
+            value = float(value)
+    except ValueError:
+        return ''
+
+    color = 'green'
+
+    if 75 < value < 85:
+        color = 'orange'
+    elif 20 < value < 75:
+        color = 'red'
+    elif value < 20:
+        color = 'yellow'
+
+    return color
+
+
+@register.assignment_tag
+def calc_percentage(numerator, denominator):
+    try:
+        denominator = float(denominator)
+        numerator = float(numerator)
+    except ValueError:
+        return ''
+
+    if denominator == 0:
+        return ''
+
+    return '{:.0%}'.format(numerator / denominator)
