8c37edcb8cede3655b907972dcf36d210b66acb5
Dickson Ukang'a <ukanga@gmail.com>
2016-09-08 04:02:30 -0400

    load_osm_hh loads osm files into the Hoesehold model

--- mspray/apps/main/management/commands/load_osm_hh.py
+++ mspray/apps/main/management/commands/load_osm_hh.py
@@ -1,66 +1,69 @@
-import csv
 import os
 
+from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
 from mspray.apps.main.osm import parse_osm
+from mspray.apps.main.models import Household
+from mspray.apps.main.models import Location
+
+
+def process_osm_file(path):
+    with open(path) as f:
+        content = f.read()
+        nodes = parse_osm(content.strip())
+        ways = [
+            way for way in nodes
+            if not way.get('osm_id').startswith('-')
+            and way.get('osm_type') == 'way'
+        ]
+        if ways:
+            for way in ways:
+                location = Location.objects.filter(
+                    geom__contains=ways[0].get('geom'),
+                    level='ta'
+                ).first()
+                if location:
+                    try:
+                        Household.objects.create(
+                            hh_id=way.get('osm_id'),
+                            geom=way.get('geom').centroid,
+                            bgeom=way.get('geom'),
+                            data=way.get('tags'),
+                            location=location
+                        )
+                    except Location.DoesNotExist:
+                        pass
+                    except IntegrityError:
+                        pass
 
 
 class Command(BaseCommand):
     help = _('Link spray operator ans team leader to spray data.')
 
     def add_arguments(self, parser):
-        parser.add_argument(dest='osmfile', help='osm file')
+        parser.add_argument(dest='osmfolder',
+                            help='osm file')
 
     def handle(self, *args, **options):
-        osmfile = options.get('osmfile')
-        path = '/tmp/osmdata.csv'
+        osmfile = options.get('osmfolder')
         if osmfile:
-            if os.path.exists(path):
-                os.remove(path)
-            with open(path, 'w', newline='') as csv_file:
-                writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
-                headers = [
-                    'target_area', 'osm_id', 'osm', 'building', 'x', 'y',
-                    'shape_area', 'shape_leng', 'wkt'
-                ]
-                writer.writerow(headers)
-                for entry in os.scandir(os.path.dirname(osmfile)):
+            if os.path.isdir(osmfile):
+                entries = os.scandir(os.path.dirname(osmfile))
+                for entry in entries:
                     is_osm_file = entry.name.endswith('osm')
                     is_file = entry.is_file()
                     if not is_osm_file or (is_osm_file and not is_file):
                         continue
-                    self.stdout.write(entry.name)
-
-                    with open(entry.path) as f:
-                        content = f.read()
-                        nodes = parse_osm(content.strip())
-                        nodes = list(filter(
-                            lambda x:
-                            x.get('osm_type') == 'node',
-                            nodes
-                        ))
-                        ta = entry.name.replace('.osm', '')[3:]
-
-                        for node in nodes:
-                            geom = node.get('geom')
-                            tags = node.get('tags')
-                            building = tags.get('id2')
-                            shape_area = tags.get('Shape_Area')
-                            shape_length = tags.get('Shape_Leng')
-                            osm_id = node.get('osm_id')
-                            osm_type = node.get('osm_type')
-                            osm_name = 'OSM{}{}'.format(osm_type, osm_id)
-                            ta = tags.get('id') or ta
-
-                            writer.writerow([
-                                ta,
-                                osm_id,
-                                osm_name,
-                                building,
-                                geom.centroid.x,
-                                geom.centroid.y,
-                                shape_area,
-                                shape_length,
-                                geom.wkt
-                            ])
+                    count = Location.objects.count()
+                    process_osm_file(entry.path)
+                    after_count = Location.objects.count()
+                    self.stdout.write('%d structures added.' %
+                                      after_count - count)
+            else:
+                path = os.path.abspath(osmfile)
+                count = Location.objects.count()
+                process_osm_file(path)
+                after_count = Location.objects.count()
+                self.stdout.write('%d structures added.' %
+                                  (after_count - count))
