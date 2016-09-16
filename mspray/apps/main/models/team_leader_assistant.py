2f1cd360fcef177fd36fe37f2c99f93efe14393a
ivermac <mark.ekisa@gmail.com>
2016-09-16 10:39:44 +0300

    ME: added team_leader_assistant model

---
+++ mspray/apps/main/models/team_leader_assistant.py
@@ -0,0 +1,15 @@
+from django.contrib.gis.db import models
+
+
+class TeamLeaderAssistant(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=255, db_index=1)
+    location = models.ForeignKey('Location', null=True)
+    data_quality_check = models.BooleanField(default=False)
+    average_spray_quality_score = models.FloatField(default=0.0)
+
+    class Meta:
+        app_label = 'main'
+
+    def __str__(self):
+        return '{}'.format(self.name)
