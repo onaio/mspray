3275d451fc89f3a9c89dc33aa9ba7844850937b9
Dickson Ukang'a <ukanga@gmail.com>
2016-09-18 22:41:32 +0200

    management command should expect formid, remove hard coded value

--- mspray/apps/main/management/commands/fetch_dosf_data.py
+++ mspray/apps/main/management/commands/fetch_dosf_data.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
 
@@ -6,24 +5,23 @@ from mspray.apps.main.ona import fetch_form_data
 from mspray.apps.main.utils import add_directly_observed_spraying_data
 from mspray.apps.main.models import DirectlyObservedSprayingForm
 
-FORMID = getattr(settings, 'ONA_FORM_PK', 0)
-
 
 class Command(BaseCommand):
     help = _('Fetch data from ona server.')
 
     def add_arguments(self, parser):
-        parser.add_argument('--formid', dest='formid', default=FORMID,
-                            type=int, help='form id')
+        parser.add_argument('formid', type=int, help='form id')
 
     def handle(self, *args, **options):
-        formid = 145904
+        formid = int(options['formid'])
         latest = DirectlyObservedSprayingForm.objects.filter().order_by(
-            'submission_id'
+            '-submission_id'
         ).values_list(
             'submission_id', flat=True
         ).first()
         data = fetch_form_data(formid, latest)
+        pre_count = DirectlyObservedSprayingForm.objects.count()
         for a in data:
             add_directly_observed_spraying_data(a)
-        print("successfully imported data")
+        count = DirectlyObservedSprayingForm.objects.count()
+        print("successfully imported %d data" % (count - pre_count))
