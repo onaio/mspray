eb0977d8be4b7defae06791f8d00e02a20d752a8
ivermac <mark.ekisa@gmail.com>
2016-09-19 11:26:24 +0300

    ME: added management command to load team leader assistants

---
+++ mspray/apps/main/management/commands/load_team_leader_assistants.py
@@ -0,0 +1,39 @@
+import codecs
+import csv
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import gettext as _
+
+from mspray.apps.main.models import Location
+from mspray.apps.main.models import TeamLeaderAssistant
+
+
+class Command(BaseCommand):
+    args = '<path to team leaders csv with columns code|name>'
+    help = _('Load team leaders')
+
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            raise CommandError(_('Missing locations csv file path'))
+        for path in args:
+            try:
+                path = os.path.abspath(path)
+            except Exception as e:
+                raise CommandError(_('Error: %(msg)s' % {"msg": e}))
+            else:
+                with codecs.open(path, encoding='utf-8') as f:
+                    csv_reader = csv.DictReader(f)
+                    for row in csv_reader:
+                        team_leader, created = \
+                            TeamLeaderAssistant.objects.get_or_create(
+                                code=row['code'].strip(),
+                                name=row['name']
+                            )
+                        if created:
+                            print(row)
+                        district = row['district'].strip()
+                        if district:
+                            location = Location.objects.get(code=district)
+                            team_leader.location = location
+                            team_leader.save()
