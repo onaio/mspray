b09b45a8121f065c33c9ac26ba524a2311233595
ivermac <mark.ekisa@gmail.com>
2016-09-20 12:24:29 +0300

    ME: using BigInteger and Polygon fields for osm related models

--- mspray/apps/main/models/osm.py
+++ mspray/apps/main/models/osm.py
@@ -3,13 +3,12 @@ from django.contrib.postgres.fields import JSONField
 
 
 class Node(models.Model):
-    node_id = models.CharField(unique=True, max_length=25)
+    node_id = models.BigIntegerField(unique=True)
     version = models.IntegerField()
     changeset = models.IntegerField()
     user = models.CharField(max_length=50, null=True)
     timestamp = models.DateTimeField(null=True)
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    geom = models.PointField(srid=4326, null=True)
     tags = JSONField(default={}, null=True)
 
     class Meta:
@@ -17,13 +16,13 @@ class Node(models.Model):
 
 
 class Way(models.Model):
-    way_id = models.CharField(unique=True, max_length=25)
+    way_id = models.BigIntegerField(unique=True)
     action = models.CharField(max_length=20, null=True)
     version = models.IntegerField()
     changeset = models.IntegerField()
     user = models.CharField(max_length=50, null=True)
     timestamp = models.DateTimeField(null=True)
-    nd_path = models.CharField(max_length=255, null=True)
+    geom = models.PolygonField(srid=4326, null=True)
     tags = JSONField(default={}, null=True)
 
     class Meta:
