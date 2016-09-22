02de4cc047f65e4430389b75b966703a701e13a7
ivermac <mark.ekisa@gmail.com>
2016-09-22 11:55:56 +0300

    ME: added filtering by osm endpoint using 'code' query param

--- mspray/apps/main/views/osm.py
+++ mspray/apps/main/views/osm.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from mspray.apps.main.models import Way
+from mspray.apps.main.models import Way, Location
 from mspray.apps.main.serializers.osm import OsmSerializer
 from mspray.libs.renderers import OSMRenderer
 
@@ -12,6 +12,15 @@ class OsmViewSet(viewsets.ReadOnlyModelViewSet):
     renderer_classes = (OSMRenderer,)
 
     def list(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
+        code = request.query_params.get('code')
+        location = code and Location.objects.filter(code=code).first()
+        qs = self.get_queryset()
+        if code and location:
+            self.object_list = qs.filter(geom__contained=location.geom)
+        elif code and location is None:
+            self.object_list = qs.none()
+        else:
+            self.object_list = qs
+
         serializer = self.get_serializer(self.object_list, many=True)
         return Response(serializer.data)
