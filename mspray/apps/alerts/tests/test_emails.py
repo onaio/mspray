81a066de611680090388926719abf2c261efdc0e
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-10-18 14:18:04 +0300

    Test the weekly update email

--- mspray/apps/alerts/tests/test_emails.py
+++ mspray/apps/alerts/tests/test_emails.py
@@ -1,9 +1,12 @@
 from unittest.mock import patch
 
 from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
 
 from mspray.apps.main.tests.test_base import TestBase
 from mspray.apps.alerts.emails import send_weekly_update_email
+from mspray.apps.alerts.emails import weekly_update_email
 
 
 class TestEmails(TestBase):
@@ -41,3 +44,29 @@ class TestEmails(TestBase):
         self.assertEqual(email_args[0], 'Mosh <one@example.com>')
         self.assertEqual(email_args[1], district_list)
         self.assertEqual(email_args[2], totals)
+
+    def test_weekly_update_email(self):
+        """
+        Tests that an email is actually sent out and that it contains the
+        expected content
+        """
+        district_list = self._district_summary_data()
+        totals = self._district_summary_totals(district_list)
+        c = {'district_list': district_list, 'totals': totals,
+             'url': settings.MSPRAY_WEEKLY_DASHBOARD_UPDATE_URL}
+        subject = render_to_string(
+            'alerts/emails/weekly_update_subject.txt', c).replace('\n', '')
+        text_content = render_to_string(
+            'alerts/emails/weekly_update_body.txt', c)
+        html_content = render_to_string(
+            'alerts/emails/weekly_update_body.html', c).replace('\n', '')
+        memory_backend = 'django.core.mail.backends.locmem.EmailBackend'
+        with self.settings(EMAIL_BACKEND=memory_backend):
+            weekly_update_email('Mosh <one@example.com>', district_list,
+                                totals)
+            self.assertEqual(len(mail.outbox), 1)
+            email = mail.outbox[0]
+            self.assertEqual(email.subject, subject)
+            self.assertEqual(email.body, text_content)
+            self.assertEqual(email.alternatives[0][0], html_content)
+
