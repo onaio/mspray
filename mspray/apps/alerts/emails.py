1d94e3abe7b38a63f623e8356bf1f1865fa5806b
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-10-19 13:00:19 +0300

    Send weekly update email to multiple contacts at once instead of one by one

--- mspray/apps/alerts/emails.py
+++ mspray/apps/alerts/emails.py
@@ -14,15 +14,18 @@ def send_weekly_update_email():
     group_uuid = settings.RAPIDPRO_WEEKLY_UPDATE_CONTACT_GROUP
     contacts = get_contacts(group_uuid)
     district_list, totals = get_district_summary()
+    address_list = []
     for contact in contacts:
         if contact.emails:
             # we just take the first email address
             address = "{name} <{email}>".format(name=contact.name,
                                                 email=contact.emails[0])
-            weekly_update_email(address, district_list, totals)
+            address_list.append(address)
+    if address_list:
+        weekly_update_email(address_list, district_list, totals)
 
 
-def weekly_update_email(recipient, district_list, totals):
+def weekly_update_email(recipients, district_list, totals):
     """
     Creates and sends the weekly update email
     """
@@ -35,9 +38,9 @@ def weekly_update_email(recipient, district_list, totals):
     html_content = render_to_string(
         'alerts/emails/weekly_update_body.html', c).replace('\n', '')
     from_email = settings.MSPRAY_EMAIL
-    to = recipient
+    to = recipients
     reply_to = settings.MSPRAY_REPLY_TO_EMAIL
-    msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[to],
+    msg = EmailMultiAlternatives(subject=subject, body=text_content, to=to,
                                  from_email=from_email, reply_to=[reply_to])
     msg.attach_alternative(html_content, "text/html")
     return msg.send(fail_silently=False)
