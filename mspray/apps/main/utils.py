aca32de8a817e7c6aaedfe2100837e2429e10f6e
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-10-20 11:06:52 +0300

    Fix very silly bug

--- mspray/apps/main/utils.py
+++ mspray/apps/main/utils.py
@@ -221,7 +221,7 @@ def add_spray_data(data):
 
     # run tasks after creating SprayDay obj
     if sprayday.has_osm_data():
-        run_tasks_after_spray_data(spray_date)
+        run_tasks_after_spray_data(sprayday)
 
     return sprayday
 
