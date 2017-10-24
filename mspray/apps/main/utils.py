64b19344f6159419130d44852c34ea784e22fbc8
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-10-24 17:56:59 +0300

    Better get_or_create for SprayDay

--- mspray/apps/main/utils.py
+++ mspray/apps/main/utils.py
@@ -188,10 +188,14 @@ def add_spray_data(data):
 
     sprayday, created = SprayDay.objects.get_or_create(
         submission_id=submission_id,
-        spray_date=spray_date,
-        geom=geom,
-        location=location
+        spray_date=spray_date
     )
+
+    if created:
+        if geom is not None:
+            sprayday.geom = geom
+        if location is not None:
+            sprayday.location = location
 
     sprayday.data = data
 
