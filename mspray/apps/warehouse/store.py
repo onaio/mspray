11b233742aef2d8d0325525cb3e1112b7b9e408c
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-11-07 15:49:57 +0300

    Create components of a way to ingest historical data to Druid
    - Use django-storages to save druid-compatible data to s3
    - Use Druid index task to ingest data from s3 url(s)
    - remove un-needed houshold druid stuff

--- mspray/apps/warehouse/store.py
+++ mspray/apps/warehouse/store.py
@@ -1,40 +1,55 @@
+import os
 import time
 
+from django.core.files.storage import default_storage
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 from rest_framework.renderers import JSONRenderer
 
-from mspray.apps.main.models import Household, SprayDay
-from mspray.apps.warehouse.serializers import HouseHoldDruidSerializer
+from mspray.apps.main.models import SprayDay
 from mspray.apps.warehouse.serializers import SprayDayDruidSerializer
 from mspray.apps.main.utils import queryset_iterator
 
 
-household_dir_path = getattr(settings, 'HOUSEHOLD_DRUID_DATA_DIRECTORY',
-                             '/tmp/household/')
 sprayday_dir_path = getattr(settings, 'SPRAYDAY_DRUID_DATA_DIRECTORY',
-                            '/tmp/sprayday/')
+                            'sprayday/')
 
 
-def create_household_druid_json_files(queryset=None, path=household_dir_path):
-    if not queryset:
-        queryset = Household.objects.all().order_by('id')
-    epoch = int(time.time())
-    filename = "{}household_{}.json".format(path, epoch)
-    for household in queryset_iterator(queryset, 1000):
-        data = HouseHoldDruidSerializer(household).data
-        line = JSONRenderer().render(data)
-        with open(filename, "ab") as f:
-            f.write(line + b'\n')
+def get_historical_data(day=None, month=None, year=None):
+    if any([year, month, day]):
+        path = "/".join([str(x) for x in [year, month, day] if x is not None])
+        filename = "{}/".format(settings.DRUID_SPRAYDAY_DATASOURCE) + path +\
+            "/sprayday.json"
+        queryset = SprayDay.objects.all()
+        if day:
+            queryset = queryset.filter(spray_date__day=day)
+        if month:
+            queryset = queryset.filter(spray_date__month=month)
+        if year:
+            queryset = queryset.filter(spray_date__year=year)
+        if queryset:
+            return create_sprayday_druid_json_file(queryset=queryset,
+                                                   filename=filename)
 
 
-def create_sprayday_druid_json_files(queryset=None, path=sprayday_dir_path):
+def create_sprayday_druid_json_file(queryset=None, filename=None,
+                                    path=sprayday_dir_path):
     if not queryset:
         queryset = SprayDay.objects.all().order_by('spray_date')
     epoch = int(time.time())
-    filename = "{}sprayday_{}.json".format(path, epoch)
+    if filename is None:
+        filename = "{}sprayday_{}.json".format(path, epoch)
+    if isinstance(default_storage, FileSystemStorage):
+        filename = os.path.join(default_storage.base_location, filename)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+    lines = []
     for record in queryset_iterator(queryset, 1000):
         data = SprayDayDruidSerializer(record).data
         line = JSONRenderer().render(data)
-        with open(filename, "ab") as f:
-            f.write(line + b'\n')
+        lines.append(line)
+    content = b'\n'.join(lines)
+    file = default_storage.open(filename, "wb")
+    file.write(content)
+    file.close()
+    return filename
