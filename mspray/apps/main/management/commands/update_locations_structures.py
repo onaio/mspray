e3705e79992222ec2f91269794b8a7a9dd3d56c8
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-11-24 12:02:21 +0300

    Update management command to update_locations_structures

--- mspray/apps/main/management/commands/update_locations_structures.py
+++ mspray/apps/main/management/commands/update_locations_structures.py
@@ -3,7 +3,7 @@ from django.db.models import Count
 from django.db.models import Sum
 from django.utils.translation import gettext as _
 
-from mspray.apps.main.models import Location
+from mspray.apps.main.models import Location, Household
 
 
 class Command(BaseCommand):
@@ -20,23 +20,23 @@ class Command(BaseCommand):
                     loc.save()
 
         def _update_spray_areas_structures():
-            for loc in Location.objects.filter(level='ta')\
-                    .annotate(num_structures=Count('household'))\
-                    .filter(num_structures__gt=0).iterator():
-                loc.structures = loc.num_structures
+            for loc in Location.objects.filter(level='ta'):
+                num_structures = Household.objects.filter(location=loc).count()
+                loc.structures = num_structures
                 loc.save()
 
         def _update_num_of_spray_areas():
-            for loc in Location.objects.filter(level='RHC')\
-                    .annotate(nm_ta=Count('location')):
-                loc.num_of_spray_areas = loc.nm_ta
+            for loc in Location.objects.filter(level='RHC'):
+                num_ta = loc.get_children().filter(level='ta').count()
+                loc.num_of_spray_areas = num_ta
                 loc.save()
 
-            for loc in Location.objects.filter(level='RHC')\
-                    .values('parent').annotate(num_ta=Count('location')):
-                district = Location.objects.get(pk=loc.get('parent'))
-                district.num_of_spray_areas = loc.get('num_ta')
-                district.save()
+                district = loc.parent
+                if district:
+                    district_num_ta = district.get_descendants().filter(
+                        level='ta').count()
+                    district.num_of_spray_areas = district_num_ta
+                    district.save()
 
         _update_spray_areas_structures()
         for level in ['RHC', 'district']:
