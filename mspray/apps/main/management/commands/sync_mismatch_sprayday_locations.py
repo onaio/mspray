d6bf51bdb4311b0abf6405e03a3e67ed8ab5b413
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-12-04 18:08:58 +0300

    Add management command to find SprayDay objects with mismatched locations and fix them

---
+++ mspray/apps/main/management/commands/sync_mismatch_sprayday_locations.py
@@ -0,0 +1,19 @@
+import gc
+
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+from mspray.apps.main.utils import get_spraydays_with_mismatched_locations
+from mspray.apps.main.utils import queryset_iterator
+from mspray.apps.main.tasks import link_spraypoint_with_osm
+
+
+class Command(BaseCommand):
+    help = _('Find SprayDay objects with mismatched locations and re-link '
+             'with OSM data')
+
+    def handle(self, *args, **options):
+        queryset = get_spraydays_with_mismatched_locations()
+        for record in queryset_iterator(queryset):
+            link_spraypoint_with_osm.delay(record.pk)
+            gc.collect()
