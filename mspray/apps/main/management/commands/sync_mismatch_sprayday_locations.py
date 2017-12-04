fe97f81bd648e50aabed0603e0b9e4974f106efd
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-12-04 18:14:45 +0300

    Add prints to give commandline feedback

--- mspray/apps/main/management/commands/sync_mismatch_sprayday_locations.py
+++ mspray/apps/main/management/commands/sync_mismatch_sprayday_locations.py
@@ -14,6 +14,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         queryset = get_spraydays_with_mismatched_locations()
+        print("{} mismatched records".format(queryset.count()))
         for record in queryset_iterator(queryset):
+            print("SprayDay ID: {} ".format(record.pk))
             link_spraypoint_with_osm.delay(record.pk)
             gc.collect()
