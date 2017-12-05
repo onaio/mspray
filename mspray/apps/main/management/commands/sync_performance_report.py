804abe4675b94de7030a3fe2771dd1c737fe5a82
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-12-05 10:57:02 +0300

    code cleanup

--- mspray/apps/main/management/commands/sync_performance_report.py
+++ mspray/apps/main/management/commands/sync_performance_report.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from mspray.apps.main.models import SprayDay
 from mspray.apps.main.utils import find_missing_performance_report_records
 from mspray.apps.main.utils import performance_report
+from mspray.apps.main.utils import queryset_iterator
 
 
 class Command(BaseCommand):
@@ -16,5 +17,5 @@ class Command(BaseCommand):
             data__sprayformid__in=missing_sprayformids).distinct(
             'spray_operator')
 
-        for q in queryset.objects.all():
-            performance_report(q.spray_operator)
+        for record in queryset_iterator(queryset):
+            performance_report(record.spray_operator)
