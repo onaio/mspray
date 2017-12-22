9df4bd103ebc0a541bf5be3780005469b4c9504d
Kelvin Jayanoris <kjayanoris@gmail.com>
2017-12-22 12:16:53 +0300

    Modify the remove_duplicate_households command to accept a 'field' argument.
    The field argument can either be 'geom' or 'bgeom' and is used to find
    duplicates based on either of those fields.
    
    Effective duplicate removal is to be done in two rounds, once based on the bgeom
    field and then based on the geom field.

--- mspray/apps/main/management/commands/remove_duplicate_households.py
+++ mspray/apps/main/management/commands/remove_duplicate_households.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import gettext as _
 from django.db.models import Count
 
@@ -14,21 +14,38 @@ class Command(BaseCommand):
     """
     help = _("Remove duplicate Household objects based on 'geom' field")
 
+    allowed_fields = ['geom', 'bgeom']
+
+    def add_arguments(self, parser):
+        parser.add_argument('--field', dest='field', default='bgeom',
+                            help='the field to use to get duplicates')
+
     def handle(self, *args, **options):
-        location_with_duplicates = Household.objects.filter().values('geom')\
+        try:
+            field = options['field']
+        except KeyError:
+            raise CommandError(_('Please specify field to use'))
+        else:
+            if field not in self.allowed_fields:
+                raise CommandError(_("Only 'geom' or 'bgeom' fields are "
+                                     "supported at this time"))
+
+        location_with_duplicates = Household.objects.filter().values(field)\
             .annotate(k=Count('id'))\
             .filter(k__gt=1)\
             .values_list('location', flat=True).distinct()
         for location_id in location_with_duplicates:
             hh_dupes = Household.objects.filter(location_id=location_id)\
-                .values('geom').annotate(k=Count('id')).filter(k__gt=1)
+                .values(field).annotate(k=Count('id')).filter(k__gt=1)
 
             total_deleted = 0
             total = hh_dupes.count()
+
             for household in hh_dupes:
-                matching_hh = Household.objects.filter(geom=household['geom'])
+                filter_kwargs = {field: household[field]}
+                matching_hh = Household.objects.filter(**filter_kwargs)
                 if matching_hh.count() > 1:
-                    osmids = Household.objects.filter(geom=household['geom'])\
+                    osmids = Household.objects.filter(**filter_kwargs)\
                         .values('hh_id')
                     submissions = SprayDay.objects.filter(osmid__in=osmids)\
                         .values_list('osmid', flat=True).order_by('spray_date')
