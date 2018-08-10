b7b193c32b27c9c91ab71690d6668d24a7b62dbd
Dickson Ukang'a <ukanga@gmail.com>
2018-08-10 16:28:32 +0300

    Initial map view for trials

--- mspray/apps/trials/views.py
+++ mspray/apps/trials/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Trials views"""
+import json
 from collections import OrderedDict
+from django.conf import settings
 
 from django.contrib.postgres.fields.jsonb import KeyTextTransform
 from django.db import models
@@ -9,7 +11,29 @@ from django.db.models.functions import Cast
 from django.shortcuts import get_object_or_404, render
 
 from mspray.apps.main.models import Location
+from mspray.apps.main.serializers.target_area import (
+    GeoTargetAreaSerializer, TargetAreaQuerySerializer,
+    TargetAreaSerializer, count_duplicates, get_duplicates)
 from mspray.apps.trials.models import Sample
+from mspray.apps.main.utils import get_location_dict, parse_spray_date
+from mspray.apps.main.views.target_area import (TargetAreaHouseholdsViewSet,
+                                                TargetAreaViewSet)
+
+from mspray.apps.main.mixins import SiteNameMixin
+from django.views.generic import DetailView
+from mspray.apps.main.query import get_location_qs
+NOT_SPRAYABLE_VALUE = settings.NOT_SPRAYABLE_VALUE
+
+
+def get_level(level):
+    """Returns the level below the level specified, defaults to District.
+    """
+    if level.lower() == 'district':
+        return 'Site'
+    elif level.lower() == 'ta' or level.lower() == 'site':
+        return 'Household'
+
+    return 'District'
 
 
 def get_samples_for_method(queryset, collection_method, key, key_name):
@@ -173,13 +197,104 @@ def site(request, site_id):
         'title': "%s: %s" % (level, location.name)
     })
 
+# def site_map(request, site_id):
+#     """Spray Areas in a district trials view.
+#     """
+#     surveys = Sample.objects.values_list(
+#         'visit', flat=True).order_by('visit').distinct()
 
-def get_level(level):
-    """Returns the level below the level specified, defaults to District.
-    """
-    if level.lower() == 'district':
-        return 'Site'
-    elif level.lower() == 'ta' or level.lower() == 'site':
-        return 'Household'
+#     location = get_object_or_404(Location, pk=site_id)
+#     site_kwargs = {
+#         'district' if location.level == 'district' else 'spray_area': location
+#     }
+#     level = 'Site' if location.level == 'ta' else location.level.title()
+#     return render(request, 'trials/map.html', {
+#         'trials': True,
+#         'level': get_level(level),
+#         'title': "%s: %s" % (level, location.name)
+#     })
 
-    return 'District'
+
+class SiteMapView(SiteNameMixin, DetailView):
+    """Site Map view"""
+    template_name = 'home/map.html'
+    model = Location
+    slug_field = 'pk'
+
+    def get_queryset(self):
+        queryset = super(SiteMapView, self).get_queryset()
+
+        return get_location_qs(queryset)
+
+    def get_context_data(self, **kwargs):
+        context = super(SiteMapView, self).get_context_data(**kwargs)
+        serializer_class = TargetAreaQuerySerializer \
+            if settings.SITE_NAME == 'namibia' else TargetAreaSerializer
+        location = context['object']
+        if location.level == 'RHC':
+            location = get_location_qs(
+                Location.objects.filter(pk=location.pk),
+                'RHC'
+            ).first()
+        serializer = serializer_class(location,
+                                      context={'request': self.request})
+        context['target_data'] = serializer.data
+        spray_date = parse_spray_date(self.request)
+        if spray_date:
+            context['spray_date'] = spray_date
+        if settings.MSPRAY_SPATIAL_QUERIES or \
+                context['object'].geom is not None:
+            view = TargetAreaViewSet.as_view({'get': 'retrieve'})
+            response = view(self.request, pk=context['object'].pk,
+                            format='geojson')
+            response.render()
+            context['not_sprayable_value'] = NOT_SPRAYABLE_VALUE
+            context['ta_geojson'] = response.content
+            bgeom = settings.HH_BUFFER and settings.OSM_SUBMISSIONS
+
+            if self.object.level in ['district', 'RHC']:
+                data = GeoTargetAreaSerializer(
+                    get_location_qs(self.object.location_set.all(),
+                                    self.object.level),
+                    many=True,
+                    context={'request': self.request}
+                ).data
+                context['hh_geojson'] = json.dumps(data)
+            else:
+                loc = context['object']
+                hhview = TargetAreaHouseholdsViewSet.as_view({
+                    'get': 'retrieve'
+                })
+                response = hhview(
+                    self.request,
+                    pk=loc.pk,
+                    bgeom=bgeom,
+                    spray_date=spray_date,
+                    format='geojson'
+                )
+                response.render()
+                context['hh_geojson'] = response.content
+                sprayed_duplicates = list(
+                    get_duplicates(loc, True, spray_date))
+                not_sprayed_duplicates = list(
+                    get_duplicates(loc, False, spray_date))
+                context['sprayed_duplicates_data'] = json.dumps(
+                    sprayed_duplicates
+                )
+                context['sprayed_duplicates'] = count_duplicates(loc, True,
+                                                                 spray_date)
+                context['not_sprayed_duplicates_data'] = json.dumps(
+                    not_sprayed_duplicates
+                )
+                context['not_sprayed_duplicates'] = \
+                    count_duplicates(loc, False)
+
+        context['districts'] = Location.objects.filter(parent=None)\
+            .values_list('id', 'code', 'name').order_by('name')
+
+        context.update({'map_menu': True})
+        context.update(get_location_dict(self.object.pk))
+        context['not_sprayed_reasons'] = json.dumps(
+            settings.MSPRAY_UNSPRAYED_REASON_OTHER)
+
+        return context
