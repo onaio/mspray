bf81c4d4f128ee6537ae51e4bac1ec21b7ee3b21
Dickson Ukang'a <ukanga@gmail.com>
2018-10-18 16:51:52 +0300

    Update test

--- mspray/apps/main/tests/test_views.py
+++ mspray/apps/main/tests/test_views.py
@@ -11,15 +11,14 @@ from mspray.apps.main.models.spray_day import DATA_ID_FIELD
 
 
 class TestViews(TestBase):
-
     def setUp(self):
         TestBase.setUp(self)
         app.conf.update(CELERY_ALWAYS_EAGER=True)
 
-    @patch('mspray.apps.main.tasks.fetch_osm_xml')
-    @patch('mspray.apps.main.utils.run_tasks_after_spray_data')
+    @patch("mspray.apps.main.tasks.fetch_osm_xml")
+    @patch("mspray.apps.main.utils.run_tasks_after_spray_data")
     def test_create_sprayday(self, tasks_mock, osm_mock):
-        view = SprayDayViewSet.as_view({'post': 'create'})
+        view = SprayDayViewSet.as_view({"post": "create"})
 
         osm_mock.return_value = OSMXML.strip()
 
@@ -30,15 +29,16 @@ class TestViews(TestBase):
         self.assertTrue(osm_mock.called)
         self.assertFalse(tasks_mock.called)
 
-    @patch('mspray.apps.main.tasks.fetch_osm_xml')
-    @patch('mspray.apps.main.utils.run_tasks_after_spray_data')
+    @patch("mspray.apps.main.tasks.fetch_osm_xml")
+    @patch("mspray.apps.main.utils.run_tasks_after_spray_data")
     def test_update_sprayday(self, tasks_mock, osm_mock):
-        view = SprayDayViewSet.as_view({'post': 'create'})
+        view = SprayDayViewSet.as_view({"post": "create"})
 
         # get submission data that has OSM info
         self._load_fixtures()
         sp = SprayDay.objects.filter(
-                data__has_key=settings.MSPRAY_OSM_PRESENCE_FIELD).first()
+            data__has_key=settings.MSPRAY_OSM_PRESENCE_FIELD
+        ).first()
         data_osm = sp.data.copy()
         data_no_osm = sp.data.copy()
 
@@ -47,7 +47,7 @@ class TestViews(TestBase):
         data_no_osm[DATA_ID_FIELD] = 111
 
         # first send the request with no osm
-        osm_fields = [x for x in data_osm.keys() if 'osmstructure:' in x]
+        osm_fields = [x for x in data_osm.keys() if "osmstructure:" in x]
         for osm_field in osm_fields:
             data_no_osm.pop(osm_field)
 
@@ -63,5 +63,5 @@ class TestViews(TestBase):
         self.assertEqual(response_osm.status_code, 201)
         self.assertTrue(tasks_mock.called)
         self.assertTrue(osm_mock.called)
-        self.assertEqual(osm_mock.call_count, 2)
+        self.assertEqual(osm_mock.call_count, 1)
         self.assertEqual(tasks_mock.call_count, 1)
