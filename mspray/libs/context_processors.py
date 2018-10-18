2c3dc652373c423cfeeb6498c4fd7faa645ec101
Dickson Ukang'a <ukanga@gmail.com>
2018-10-18 16:15:21 +0300

    Default google_settings to an empty string

--- mspray/libs/context_processors.py
+++ mspray/libs/context_processors.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
+"""context processors.
+"""
 from django.conf import settings
 
 
 def google_settings(request):
-    return {
-        'GOOGLE_API_KEY': settings.GOOGLE_API_KEY
-    }
+    """Set GOOGLE_API_KEY"""
+    return {"GOOGLE_API_KEY": getattr(settings, "GOOGLE_API_KEY", "")}
 
 
 def mspray_settings(request):
+    """Set WAS_SPRAYED_VALUE and WAS_NOT_SPRAYED_VALUE"""
     return {
-        'WAS_SPRAYED_VALUE':
-        getattr(settings, 'MSPRAY_WAS_SPRAYED_VALUE', 'yes'),
-        'WAS_NOT_SPRAYED_VALUE':
-        getattr(settings, 'MSPRAY_WAS_NOT_SPRAYED_VALUE', 'no')
+        "WAS_SPRAYED_VALUE": getattr(
+            settings, "MSPRAY_WAS_SPRAYED_VALUE", "yes"
+        ),
+        "WAS_NOT_SPRAYED_VALUE": getattr(
+            settings, "MSPRAY_WAS_NOT_SPRAYED_VALUE", "no"
+        ),
     }
