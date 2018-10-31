972606cc822599031cd7fc67e26cababc29eee40
Dickson Ukang'a <ukanga@gmail.com>
2018-10-31 14:39:59 +0300

    Format mopup decision date column

--- mspray/apps/main/templatetags/progress_extras.py
+++ mspray/apps/main/templatetags/progress_extras.py
@@ -4,12 +4,15 @@ Custom template filters and tags used by the spray indicators.
 """
 from django import template
 
+from dateutil import parser
+
 register = template.Library()  # pylint: disable=invalid-name
 
 GREEN = "green"
 ORANGE = "orange"
 RED = "red"
 YELLOW = "yellow"
+NO_DECISION_FORM = "No decision form"
 
 
 @register.filter
@@ -91,5 +94,27 @@ def structures_mopup_colour(value):
     except TypeError:
         # ignore if we got a string instead
         pass
+
+    return ""
+
+
+@register.filter
+def decision_date(value, arg):
+    """Return the decision date if arg is within 2 days of the value."""
+    if value and arg:
+        the_date = parser.parse(value).date()
+        days = (the_date - arg).days
+
+        if days in range(0, 3):
+            return value
+
+    return NO_DECISION_FORM
+
+
+@register.filter
+def decision_date_colour(value, arg):
+    """Return RED if arg is within 2 days of the value."""
+    if decision_date(value, arg) == NO_DECISION_FORM:
+        return RED
 
     return ""
--- mspray/apps/main/tests/templatetags/test_progress_extras.py
+++ mspray/apps/main/tests/templatetags/test_progress_extras.py
@@ -2,12 +2,15 @@
 """
 Test progress_extras templatetags.
 """
+import datetime
 from unittest import TestCase
 
 from mspray.apps.main.templatetags.progress_extras import (
     GREEN,
     ORANGE,
     RED,
+    decision_date,
+    decision_date_colour,
     sprayed_effectively_color,
     structures_mopup_colour,
 )
@@ -30,3 +33,36 @@ class TestProgressExtras(TestCase):
         self.assertEqual(structures_mopup_colour(2), GREEN)
         self.assertEqual(structures_mopup_colour(12), ORANGE)
         self.assertEqual(structures_mopup_colour(24), RED)
+
+    def test_decision_date(self):
+        """Test decision_date filter."""
+        # less than 2 days after the last visit date
+        self.assertEqual(
+            decision_date("2018-09-21", datetime.date(2018, 9, 20)),
+            "2018-09-21",
+        )
+        # more than 2 days after the last visit date
+        self.assertEqual(
+            decision_date("2018-09-25", datetime.date(2018, 9, 20)),
+            "No decision form",
+        )
+        # date of decision form is before the last visit date
+        self.assertEqual(
+            decision_date("2018-09-15", datetime.date(2018, 9, 20)),
+            "No decision form",
+        )
+
+    def test_decision_date_colour(self):
+        """Test decision_date_colour filter."""
+        # less than 2 days after the last visit date
+        self.assertEqual(
+            decision_date_colour("2018-09-21", datetime.date(2018, 9, 20)), ""
+        )
+        # more than 2 days after the last visit date
+        self.assertEqual(
+            decision_date_colour("2018-09-25", datetime.date(2018, 9, 20)), RED
+        )
+        # date of decision form is before the last visit date
+        self.assertEqual(
+            decision_date_colour("2018-09-15", datetime.date(2018, 9, 20)), RED
+        )
