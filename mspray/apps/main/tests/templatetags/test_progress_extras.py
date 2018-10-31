bc5ec1491f850f7628621ee8a9841aff51688817
Dickson Ukang'a <ukanga@gmail.com>
2018-10-31 14:02:14 +0300

    Test structures_mopup_colour filter

--- mspray/apps/main/tests/templatetags/test_progress_extras.py
+++ mspray/apps/main/tests/templatetags/test_progress_extras.py
@@ -4,17 +4,29 @@ Test progress_extras templatetags.
 """
 from unittest import TestCase
 
-from mspray.apps.main.templatetags.progress_extras import \
-    sprayed_effectively_color
+from mspray.apps.main.templatetags.progress_extras import (
+    GREEN,
+    ORANGE,
+    RED,
+    sprayed_effectively_color,
+    structures_mopup_colour,
+)
 
 
 class TestProgressExtras(TestCase):
     """
     Test progress_extras templatetags functions.
     """
+
     def test_sprayed_effectively_color(self):
         """
         Test sprayed_effectively_color
         """
         value = 90
-        self.assertEqual(sprayed_effectively_color(value), 'green')
+        self.assertEqual(sprayed_effectively_color(value), "green")
+
+    def test_structures_mopup_colour(self):
+        """Test structures_mopup_colour filter."""
+        self.assertEqual(structures_mopup_colour(2), GREEN)
+        self.assertEqual(structures_mopup_colour(12), ORANGE)
+        self.assertEqual(structures_mopup_colour(24), RED)
