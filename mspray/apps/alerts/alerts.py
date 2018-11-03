81d52dd9f865813ae72ce5abf9d9f49c2e123162
Dickson Ukang'a <ukanga@gmail.com>
2018-11-03 11:49:44 +0300

    Add daily_spray_effictiveness() function
    Sends to RapidPro the spray effectiveness of a spray area for a given
    spray date.

---
+++ mspray/apps/alerts/alerts.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Alert functions."""
+
+
+from mspray.apps.alerts.rapidpro import start_flow
+from mspray.apps.alerts.serializers import SprayEffectivenessSerializer
+from mspray.apps.main.models import Location, SprayDay
+
+
+def daily_spray_effectiviness(flow_uuid, spray_date):
+    """Sends to RapidPro a daily spray effectiveness calculations"""
+    visited = (
+        SprayDay.objects.filter(spray_date=spray_date)
+        .exclude(location__isnull=True)
+        .values_list("location", flat=True)
+    )
+    spray_areas = Location.objects.filter(id__in=visited)
+
+    serializer = SprayEffectivenessSerializer(spray_areas, many=True)
+    for record in serializer.data:
+        start_flow(flow_uuid, payload=record)
---
+++ mspray/apps/alerts/tests/test_alerts.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Test alerts module."""
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from mspray.apps.alerts.alerts import daily_spray_effectiviness
+from mspray.apps.main.tests.utils import data_setup, load_spray_data
+
+
+class TestAlerts(TestCase):
+    """Test alerts functions"""
+
+    @patch("mspray.apps.alerts.alerts.start_flow")
+    def test_daily_spray_effectiveness(self, mocked_start_flow):
+        """Test daily_spray_effectiveness calls rapidpro"""
+        data_setup()
+        load_spray_data()
+        daily_spray_effectiviness("flow_uuid", '2018-09-20')
+        self.assertTrue(mocked_start_flow.called)
+        self.assertEqual(mocked_start_flow.call_count, 1)
