22912e9c551ed550b8658455e59bfa974426689a
WinnyTroy <kiraguwinnie@gmail.com>
2018-11-15 19:19:09 +0300

    Added tests for both before and after mobilisation and sensitisation occurs in target areas

--- mspray/apps/main/tests/views/test_targetarea.py
+++ mspray/apps/main/tests/views/test_targetarea.py
@@ -1,11 +1,9 @@
 """Test mobilisation module."""
 from django.test import RequestFactory, TestCase
 
-from mspray.apps.main.models import Mobilisation, SensitizationVisit
-from mspray.apps.main.tests.utils import (
-    MOBILISATION_VISIT_DATA, SENSITIZATION_VISIT_DATA, data_setup)
-from mspray.apps.main.views.mobilisation import MobilisationView
-from mspray.apps.main.views.sensitization_visit import SensitizationVisitView
+from mspray.apps.main.models import Location
+from mspray.apps.main.tests.utils import data_setup
+
 
 from mspray.apps.main.views.home import TargetAreaView
 
@@ -16,51 +14,58 @@ class TestTargetAreaView(TestCase):
     def test_mobilisation(self):
         """Test processing a mobilisation visit via MobilisationView."""
         data_setup()
-        data = MOBILISATION_VISIT_DATA
+        sprayarea = Location.objects.get(name='Akros_1')
         factory = RequestFactory()
-        view = MobilisationView.as_view()
-        request = factory.post("/mobilisation", data)
-        response = view(request)
-        self.assertEqual(response.status_code, 201)
-        visit = Mobilisation.objects.get(submission_id=data["_id"])
-        self.assertTrue(visit.is_mobilised)
-
         view = TargetAreaView.as_view()
+
+        # Test mobilizatiion has not been mobilised
         request = factory.get("/2/3")
         response = view(
-            request, district_pk=visit.health_facility_id,
-            slug=visit.spray_area_id)
+            request,
+            district_pk=sprayarea.parent_id, slug=sprayarea.id)
+        self.assertContains(
+            response,
+            'Mobilised? \
+            <i class="fa fa-times" style="color: red"></i>',
+            html=True)
+
+        # mobilize the location
+        sprayarea.is_mobilised = True
+        sprayarea.save()
+
+        # Test the location is mobilised
+        response = view(
+            request, district_pk=sprayarea.parent_id,
+            slug=sprayarea.id)
         self.assertContains(
             response,
             'Mobilised? \
             <i class="fa fa-check-circle" style="color: green"></i>',
             html=True)
-        self.assertContains(
-            response,
-            'Community Ready?  <i class="fa fa-times" style="color: red"></i>',
-            html=True)
 
     def test_sensitization(self):
         """Test processing a sensitiza visit via MobilisationView."""
         data_setup()
-        data = SENSITIZATION_VISIT_DATA
         factory = RequestFactory()
-        view = SensitizationVisitView.as_view()
-        request = factory.post("/sensitization_visit", data)
-        response = view(request)
-        self.assertEqual(response.status_code, 201)
-        visit = SensitizationVisit.objects.get(submission_id=data["_id"])
-        self.assertTrue(visit.is_sensitized)
-
+        sprayarea = Location.objects.get(name='Akros_1')
         view = TargetAreaView.as_view()
+
         request = factory.get("/2/3")
         response = view(
-            request, district_pk=visit.health_facility_id,
-            slug=visit.spray_area_id)
+            request,
+            district_pk=sprayarea.parent_id, slug=sprayarea.id)
         self.assertContains(
             response,
             'Mobilised? <i class="fa fa-times" style="color: red"></i>',
             html=True)
+
+        sprayarea.is_sensitized = True
+        sprayarea.save()
+
+        response = view(
+            request, district_pk=sprayarea.parent_id,
+            slug=sprayarea.id)
+
         self.assertContains(
             response,
             'Community Ready? \
