7dbf56277dd1f2c9dd746602d4b95eaecf6483d7
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-21 19:05:30 +0300

    Add skeleton of export locations module

--- mspray/apps/reveal/settings.py
+++ mspray/apps/reveal/settings.py
@@ -4,3 +4,15 @@ from django.conf import settings
 REVEAL_DATA_ID_FIELD = getattr(settings, "REVEAL_DATA_ID_FIELD", "id")
 REVEAL_DATE_FIELD = getattr(settings, "REVEAL_DATE_FIELD", "date")
 REVEAL_GPS_FIELD = getattr(settings, "REVEAL_GPS_FIELD", "location")
+REVEAL_OPENSRP_USERNAME = getattr(settings, "REVEAL_OPENSRP_USERNAME", "")
+REVEAL_OPENSRP_PASSWORD = getattr(settings, "REVEAL_OPENSRP_PASSWORD", "")
+REVEAL_OPENSRP_BASE_URL = getattr(
+    settings, "REVEAL_OPENSRP_BASE_URL",
+    "https://reveal-stage.smartregister.org/opensrp/rest/")
+REVEAL_OPENSRP_CREATE_PARENT_LOCATIONS_ENDPOINT = getattr(
+    settings, "REVEAL_OPENSRP_CREATE_PARENT_LOCATIONS_ENDPOINT",
+    "/location/add?is_jurisdiction=true"
+)
+REVEAL_OPENSRP_CREATE_STRUCTURE_LOCATIONS_ENDPOINT = getattr(
+    settings, "REVEAL_OPENSRP_CREATE_STRUCTURE_LOCATIONS_ENDPOINT",
+    "/location/add")
