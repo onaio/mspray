d430d1049a0da01f0200fb1f06e052770fb96c3d
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-23 12:42:11 +0300

    Fix url settings

--- mspray/apps/reveal/settings.py
+++ mspray/apps/reveal/settings.py
@@ -11,11 +11,11 @@ REVEAL_OPENSRP_BASE_URL = getattr(
     "https://reveal-stage.smartregister.org/opensrp/rest/")
 REVEAL_OPENSRP_CREATE_PARENT_LOCATIONS_ENDPOINT = getattr(
     settings, "REVEAL_OPENSRP_CREATE_PARENT_LOCATIONS_ENDPOINT",
-    "/location/add?is_jurisdiction=true"
+    "location/add?is_jurisdiction=true"
 )
 REVEAL_OPENSRP_CREATE_STRUCTURE_LOCATIONS_ENDPOINT = getattr(
     settings, "REVEAL_OPENSRP_CREATE_STRUCTURE_LOCATIONS_ENDPOINT",
-    "/location/add")
+    "location/add")
 REVEAL_OPENSRP_ACTIVE = getattr(settings, "REVEAL_OPENSRP_ACTIVE", "Active")
 REVEAL_DISTRICT = getattr(settings, 'REVEAL_DISTRICT', "district")
 REVEAL_OPENSRP_DISTRICT = getattr(settings, 'REVEAL_OPENSRP_DISTRICT', 0)
