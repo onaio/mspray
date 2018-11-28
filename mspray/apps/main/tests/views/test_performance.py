3feb31c952e13518da2452d387fb8888cff15e9f
WinnyTroy <kiraguwinnie@gmail.com>
2018-11-28 10:12:29 +0300

    Fix failing test

--- mspray/apps/main/tests/views/test_performance.py
+++ mspray/apps/main/tests/views/test_performance.py
@@ -4,9 +4,9 @@ Test performance views module.
 """
 
 from django.test import RequestFactory, TestCase, override_settings
+from mspray.apps.main.models import Location
 from mspray.apps.main.views.performance import (
-    DistrictPerfomanceView, TeamLeadersPerformanceView,
-    SprayOperatorSummaryView)
+    DistrictPerfomanceView, TeamLeadersPerformanceView)
 from mspray.apps.main.tests.utils import data_setup
 
 
@@ -28,9 +28,11 @@ class TestPerformanceView(TestCase):
     def test_team_leader_performance(self):
         """Test team leaders performance view."""
         data_setup()
+        sprayarea = Location.objects.get(name='Akros_1')
+
         factory = RequestFactory()
         request = factory.get("/performance/team-leaders/458")
         view = TeamLeadersPerformanceView.as_view()
-        response = view(request, slug=1)
+        response = view(request, slug=sprayarea.id)
         self.assertEqual(response.status_code, 200)
         self.assertTrue('supervisor' in str(response.render().content))
