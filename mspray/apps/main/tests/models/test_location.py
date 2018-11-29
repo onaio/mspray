bda11cf271075e10f4bbe08a8f1ad0fbdc623f70
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-29 19:29:47 +0300

    Fix broken tests

--- mspray/apps/main/tests/models/test_location.py
+++ mspray/apps/main/tests/models/test_location.py
@@ -22,13 +22,21 @@ class TestLocation(TestCase):
         """Test calculating number of health facilities that need mopup."""
         data_setup()
         lusaka = Location.objects.get(name="Lusaka", level="district")
-        self.assertEqual(lusaka.health_centers_to_mopup, 1)
+        with self.settings(MSPRAY_MOPUP_LOWER_BOUND=0.2):
+            self.assertEqual(lusaka.health_centers_to_mopup, 0)
 
     def test_spray_areas_to_mopup(self):
         """Test calculating number of spray_areas that need mopup."""
         data_setup()
-        lusaka = Location.objects.get(name="Lusaka", level="district")
-        self.assertEqual(lusaka.spray_areas_to_mopup, 2)
+        self.assertEqual(
+            Location.objects.get(name="Lusaka",
+                                 level="district").spray_areas_to_mopup, 0)
+
+        load_spray_data()
+
+        self.assertEqual(
+            Location.objects.get(name="Lusaka",
+                                 level="district").spray_areas_to_mopup, 1)
 
     def test_structures_to_mopup(self):
         """Test calculating number of structures that need mopup."""
@@ -43,7 +51,7 @@ class TestLocation(TestCase):
         self.assertEqual(akros_2.structures_to_mopup, 3)
 
         lusaka = Location.objects.get(name="Lusaka", level="district")
-        self.assertEqual(lusaka.structures_to_mopup, 15)
+        self.assertEqual(lusaka.structures_to_mopup, 3)
 
     def test_visited_sprayed(self):
         """Test return number of structures visited that were sprayed."""
@@ -55,21 +63,26 @@ class TestLocation(TestCase):
     def test_mopup_days_needed(self):
         """Test return number of mopup days needed."""
         data_setup()
-        with self.settings(MOPUP_DAYS_DENOMINATOR=5):
+        with self.settings(
+                MOPUP_DAYS_DENOMINATOR=5, MSPRAY_MOPUP_LOWER_BOUND=0.2):
             akros_2 = Location.objects.get(name="Akros_2", level="ta")
             self.assertEqual(akros_2.mopup_days_needed, 2.6)
 
+            # lusaka has just these two areas inside
             lusaka = Location.objects.get(name="Lusaka", level="district")
-            self.assertEqual(lusaka.mopup_days_needed, 5.2)
+            self.assertEqual(lusaka.mopup_days_needed, 0)
 
             # load some spray data
             load_spray_data()
+
+            akros_2.refresh_from_db()
+            lusaka.refresh_from_db()
 
             akros_2 = Location.objects.get(name="Akros_2", level="ta")
             self.assertEqual(akros_2.mopup_days_needed, 0.2)
 
             lusaka = Location.objects.get(name="Lusaka", level="district")
-            self.assertEqual(lusaka.mopup_days_needed, 2.8000000000000003)
+            self.assertEqual(lusaka.mopup_days_needed, 0.2)
 
     def test_structures_on_ground(self):
         """Test structures_on_ground"""
