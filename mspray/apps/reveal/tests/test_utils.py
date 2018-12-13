c13cf44a8ec42efd810d6fb32bd57c13ec958ea0
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-12-13 15:18:04 +0300

    Update and fix tests to match new reveal data

--- mspray/apps/reveal/tests/test_utils.py
+++ mspray/apps/reveal/tests/test_utils.py
@@ -69,7 +69,7 @@ class TestUtils(TestBase):
         self.assertEqual(1, SprayDay.objects.all().count())
 
         sprayday = SprayDay.objects.first()
-        self.assertEqual(1337, sprayday.submission_id)
+        self.assertEqual(1, sprayday.submission_id)
         self.assertEqual(date(2015, 9, 21), sprayday.spray_date)
         self.assertEqual(
             Point(float(28.35517894260948), float(-15.41818400162254)).coords,
@@ -84,16 +84,32 @@ class TestUtils(TestBase):
         self.assertTrue(sprayday.household.visited)
         self.assertTrue(sprayday.household.sprayable)
 
+        # try again to ensure we dont create duplicate records
+        add_spray_data(data=input_data)
+        self.assertEqual(1, SprayDay.objects.all().count())
+
     def test_add_spray_data_not_sprayed(self):
         """
         Test add_spray_data NOT SPRAYED for reveal
         """
         SprayDay.objects.all().delete()
         input_data = {
-            "id": 1337,
-            "date": "2015-09-21",
-            "location": "-15.41818400162254, 28.35517894260948",
-            "spray_status": "Not Sprayed - Refused"
+            'id': '1337',
+            'parent_id': '3537',
+            'status': 'Active',
+            'geometry': """{
+                "type": "Point",
+                "coordinates": [
+                    28.35517894260948,-15.41818400162254
+                ]}""",
+            'server_version': 1542970626309,
+            'task_id': '2caa810d-d4da-4e67-838b-badb9bd86e06',
+            'task_spray_operator': 'demoMTI',
+            'task_status': 'Ready',
+            'task_business_status': 'Not Sprayed - Refused',
+            'task_execution_start_date': '2015-09-21T1000',
+            'task_execution_end_date': '2015-09-21T1100',
+            'task_server_version': 1543867945196
         }
         add_spray_data(data=input_data)
         self.assertEqual(1, SprayDay.objects.all().count())
@@ -109,10 +125,22 @@ class TestUtils(TestBase):
         """
         SprayDay.objects.all().delete()
         input_data = {
-            "id": 1337,
-            "date": "2015-09-21",
-            "location": "-15.41818400162254, 28.35517894260948",
-            "spray_status": "Not Visited"
+            'id': '1337',
+            'parent_id': '3537',
+            'status': 'Active',
+            'geometry': """{
+                "type": "Point",
+                "coordinates": [
+                    28.35517894260948,-15.41818400162254
+                ]}""",
+            'server_version': 1542970626309,
+            'task_id': '2caa810d-d4da-4e67-838b-badb9bd86e06',
+            'task_spray_operator': 'demoMTI',
+            'task_status': 'Ready',
+            'task_business_status': 'Not Visited',
+            'task_execution_start_date': '2015-09-21T1000',
+            'task_execution_end_date': '2015-09-21T1100',
+            'task_server_version': 1543867945196
         }
         add_spray_data(data=input_data)
         self.assertEqual(1, SprayDay.objects.all().count())
@@ -128,10 +156,22 @@ class TestUtils(TestBase):
         """
         SprayDay.objects.all().delete()
         input_data = {
-            "id": 1337,
-            "date": "2015-09-21",
-            "location": "-15.41818400162254, 28.35517894260948",
-            "spray_status": "Not Sprayable"
+            'id': '1337',
+            'parent_id': '3537',
+            'status': 'Active',
+            'geometry': """{
+                "type": "Point",
+                "coordinates": [
+                    28.35517894260948,-15.41818400162254
+                ]}""",
+            'server_version': 1542970626309,
+            'task_id': '2caa810d-d4da-4e67-838b-badb9bd86e06',
+            'task_spray_operator': 'demoMTI',
+            'task_status': 'Ready',
+            'task_business_status': 'Not Sprayable',
+            'task_execution_start_date': '2015-09-21T1000',
+            'task_execution_end_date': '2015-09-21T1100',
+            'task_server_version': 1543867945196
         }
         add_spray_data(data=input_data)
         self.assertEqual(1, SprayDay.objects.all().count())
