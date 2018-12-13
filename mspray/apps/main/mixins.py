46ff58d0716da4f0937f04d8b10e89d9335e5948
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-12-13 19:46:47 +0300

    Code cleanup

--- mspray/apps/main/mixins.py
+++ mspray/apps/main/mixins.py
@@ -1,12 +1,14 @@
+"""
+Mixins module
+"""
 from django.conf import settings
 
 
-class SiteNameMixin(object):
+class SiteNameMixin:
     def get_context_data(self, **kwargs):
-        context = super(SiteNameMixin, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context["site_name"] = settings.SITE_NAME
         context["active_site"] = (
-            "MDA" if "mda" in self.request.get_full_path() else "IRS"
-        )
+            "MDA" if "mda" in self.request.get_full_path() else "IRS")
 
         return context
