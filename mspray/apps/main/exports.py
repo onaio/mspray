20b4bbb65e8442c8f3503b429fc748c6fcb193f2
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-01 20:24:12 +0300

    Add get_not_targeted_data

--- mspray/apps/main/exports.py
+++ mspray/apps/main/exports.py
@@ -6,8 +6,13 @@ from tempfile import TemporaryFile
 from django.core.files.base import File
 from django.core.files.storage import default_storage
 
-from mspray.apps.main.models import Location
-from mspray.apps.main.serializers.target_area import TargetAreaRichSerializer
+from mspray.apps.main.models import Location, SprayDay
+from mspray.apps.main.views.sprayday import get_not_targeted_within_geom
+from mspray.apps.main.serializers.target_area import (
+    RichQuerysetMixin,
+    SprayOperatorDailySummaryMixin,
+    TargetAreaRichSerializer,
+)
 
 
 def detailed_spray_area_data(queryset=None):
@@ -82,3 +87,48 @@ def detailed_spray_area_to_file(filename="detailed_sprayareas.csv"):
             default_storage.delete(filename)
         spray_areas_file.seek(0)
         default_storage.save(filename, spray_areas_file)
+
+
+def get_not_targeted_data(rhc: object):
+    """
+    Returns data for spray events not tied to a target area
+
+    :param rhc: the RHC location object
+    :return: dict of data
+    """
+    sprays = SprayDay.objects.filter(location=None, geom__within=rhc.geom)
+
+    spray_metrics = get_not_targeted_within_geom(rhc.geom)[0]
+
+    sop = SprayOperatorDailySummaryMixin()
+    sop.queryset = sprays
+
+    rich_data = RichQuerysetMixin()
+    rich_data.queryset = sprays
+
+    return {
+        "name": "Not in Target Area",
+        "rhc": rhc.name,
+        "district": rhc.parent.name,
+        "found": spray_metrics["found"],
+        "visited_sprayed": spray_metrics["sprayed"],
+        "visited_not_sprayed": spray_metrics["not_sprayed"],
+        "sprayed_totalpop": rich_data.get_sprayed_totalpop(),
+        "sprayed_males": rich_data.get_sprayed_males(),
+        "sprayed_females": rich_data.get_sprayed_females(),
+        "sprayed_pregwomen": rich_data.get_sprayed_pregwomen(),
+        "sprayed_childrenU5": rich_data.get_sprayed_childrenU5(),
+        "unsprayed_totalpop": rich_data.get_unsprayed_totalpop(),
+        "unsprayed_males": rich_data.get_unsprayed_males(),
+        "unsprayed_females": rich_data.get_unsprayed_females(),
+        "unsprayed_pregnant_women": rich_data.get_unsprayed_pregnant_women(),
+        "unsprayed_children_u5": rich_data.get_unsprayed_children_u5(),
+        "total_rooms": rich_data.get_total_rooms(),
+        "sprayed_rooms": rich_data.get_sprayed_rooms(),
+        "total_nets": rich_data.get_total_nets(),
+        "total_uNet": rich_data.get_total_uNet(),
+        "bottles_start": sop.get_bottles_start(),
+        "bottles_full": sop.get_bottles_full(),
+        "bottles_empty": sop.get_bottles_empty(),
+        "bottles_accounted": sop.get_bottles_accounted(),
+    }
