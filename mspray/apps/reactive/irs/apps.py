cf13fb724a28c8a5bc79c218151a9724ae89c1c2
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-27 12:55:20 +0300

    Add custom settings

--- mspray/apps/reactive/irs/apps.py
+++ mspray/apps/reactive/irs/apps.py
@@ -4,8 +4,23 @@ from django.apps import AppConfig
 
 class IRSConfig(AppConfig):
     """
-    reveal app config class
+    Reactive IRS app config class
     """
 
     name = "mspray.apps.reactive.irs"
     app_label = "reactive_irs"
+
+    def ready(self):
+        """
+        Do stuff when the app is ready
+        """
+        # pylint: disable=unused-variable
+        import mspray.apps.reactive.irs.signals  # noqa
+
+        # set up app settings
+        from django.conf import settings  # noqa
+        import mspray.apps.reactive.irs.settings as defaults  # noqa
+
+        for name in dir(defaults):
+            if name.isupper() and not hasattr(settings, name):
+                setattr(settings, name, getattr(defaults, name))
---
+++ mspray/apps/reactive/irs/settings.py
@@ -0,0 +1,3 @@
+"""Reactive IRS settings module"""
+
+MSPRAY_REACTIVE_IRS_CHW_BUFFER = 0.0005
