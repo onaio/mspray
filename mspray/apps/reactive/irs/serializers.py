9e1c05da6c91b2bf33e059b177382ac6496617f4
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-01 16:51:57 +0300

    Add CHW in target area serializers

--- mspray/apps/reactive/irs/serializers.py
+++ mspray/apps/reactive/irs/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 from mspray.apps.main.models import Location
-from mspray.apps.reactive.irs.mixins import CHWLocationMixin
+from mspray.apps.reactive.irs.mixins import (CHWinLocationMixin,
+                                             CHWLocationMixin)
 
 
 class CHWLocationSerializer(CHWLocationMixin, serializers.ModelSerializer):
@@ -49,6 +50,99 @@ class CHWLocationSerializer(CHWLocationMixin, serializers.ModelSerializer):
 
 class GeoCHWLocationSerializer(CHWLocationMixin, GeoFeatureModelSerializer):
     """Geo-aware Community Health Worker (CHW) location serializer class"""
+
+    level = serializers.ReadOnlyField()
+    name = serializers.ReadOnlyField()
+    code = serializers.ReadOnlyField()
+    district_name = serializers.SerializerMethodField()
+    structures = serializers.SerializerMethodField()
+    total_structures = serializers.SerializerMethodField()
+    num_new_structures = serializers.SerializerMethodField()
+    found = serializers.SerializerMethodField()
+    visited_total = serializers.SerializerMethodField()
+    visited_sprayed = serializers.SerializerMethodField()
+    visited_not_sprayed = serializers.SerializerMethodField()
+    visited_refused = serializers.SerializerMethodField()
+    visited_other = serializers.SerializerMethodField()
+    not_visited = serializers.SerializerMethodField()
+    bounds = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = [
+            "id",
+            "code",
+            "name",
+            "district_name",
+            "level",
+            "found",
+            "structures",
+            "visited_total",
+            "visited_sprayed",
+            "visited_not_sprayed",
+            "visited_refused",
+            "visited_other",
+            "not_visited",
+            "total_structures",
+            "num_new_structures",
+            "bounds",
+            "geom",
+        ]
+        model = Location
+        geo_field = "geom"
+
+
+class CHWinTargetAreaSerializer(CHWinLocationMixin,
+                                serializers.ModelSerializer):
+    """
+    Serializer class for a location that is aware of CHW locations within it
+    """
+
+    level = serializers.ReadOnlyField()
+    name = serializers.ReadOnlyField()
+    code = serializers.ReadOnlyField()
+    district_name = serializers.SerializerMethodField()
+    structures = serializers.SerializerMethodField()
+    total_structures = serializers.SerializerMethodField()
+    num_new_structures = serializers.SerializerMethodField()
+    found = serializers.SerializerMethodField()
+    visited_total = serializers.SerializerMethodField()
+    visited_sprayed = serializers.SerializerMethodField()
+    visited_not_sprayed = serializers.SerializerMethodField()
+    visited_refused = serializers.SerializerMethodField()
+    visited_other = serializers.SerializerMethodField()
+    not_visited = serializers.SerializerMethodField()
+    bounds = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = [
+            "id",
+            "code",
+            "name",
+            "district_name",
+            "level",
+            "found",
+            "structures",
+            "visited_total",
+            "visited_sprayed",
+            "visited_not_sprayed",
+            "visited_refused",
+            "visited_other",
+            "not_visited",
+            "total_structures",
+            "num_new_structures",
+            "bounds",
+            "geom",
+        ]
+        model = Location
+        geo_field = "geom"
+
+
+class GeoCHWinTargetAreaSerializer(CHWinLocationMixin,
+                                   GeoFeatureModelSerializer):
+    """
+    Geo Serializer class for a location that is aware of
+    CHW locations within it
+    """
 
     level = serializers.ReadOnlyField()
     name = serializers.ReadOnlyField()
