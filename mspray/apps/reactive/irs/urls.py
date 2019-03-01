bb38e8b666f7a84b7259a3a01f057fb37fd89b21
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-01 16:52:43 +0300

    Cleanup reactive irs urls

--- mspray/apps/reactive/irs/urls.py
+++ mspray/apps/reactive/irs/urls.py
@@ -1,12 +1,12 @@
 """urls module for Reactive IRS"""
 from django.urls import path
 
-from mspray.apps.reactive.irs.views import CHWLocationMapView, CHWListView
+from mspray.apps.reactive.irs.views import CHWListView, CHWLocationMapView
 
 # pylint: disable=invalid-name
 app_name = "reactive_irs"
 urlpatterns = [
     path("<int:pk>", CHWListView.as_view(), name="chw_list"),
     path("map/<int:pk>", CHWLocationMapView.as_view(), name="chw_list_map"),
-    path("chw/<int:pk>", CHWLocationMapView.as_view(), name="chw_map")
+    path("chw/map/<int:pk>", CHWLocationMapView.as_view(), name="chw_map"),
 ]
