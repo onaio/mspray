a0cc1e536f616790998562ce47d5b0d72dcdf4b7
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-01 15:25:00 +0300

    Add support for CHW list map view

--- mspray/apps/reactive/irs/views.py
+++ mspray/apps/reactive/irs/views.py
@@ -11,8 +11,10 @@ from mspray.apps.main.definitions import DEFINITIONS
 from mspray.apps.main.mixins import SiteNameMixin
 from mspray.apps.main.models import Location
 from mspray.apps.main.query import get_location_qs
+from mspray.apps.main.serializers.target_area import TargetAreaSerializer
 from mspray.apps.main.utils import parse_spray_date
-from mspray.apps.main.views.target_area import CHWHouseholdsViewSet
+from mspray.apps.main.views.target_area import (CHWHouseholdsViewSet,
+                                                TargetAreaViewSet)
 from mspray.apps.reactive.irs.serializers import (CHWLocationSerializer,
                                                   GeoCHWLocationSerializer)
 
@@ -47,32 +49,44 @@ class CHWLocationMapView(SiteNameMixin, DetailView):
         spray_date = parse_spray_date(self.request)
         loc = context["object"]
 
-        serializer = CHWLocationSerializer(
-            loc, context={"request": self.request})
+        if self.object.level == "chw":
+            serializer_class = CHWLocationSerializer
+            viewset_class = CHWLocationViewSet
 
+            hhview = CHWHouseholdsViewSet.as_view({"get": "retrieve"})
+            response = hhview(
+                self.request,
+                pk=loc.pk,
+                bgeom=bgeom,
+                spray_date=spray_date,
+                format="geojson",
+            )
+            response.render()
+            hh_geojson = response.content.decode()
+        else:
+            serializer_class = TargetAreaSerializer
+            viewset_class = TargetAreaViewSet
+
+            loc = get_location_qs(Location.objects.filter(pk=loc.pk)).first()
+            chw_objects = Location.objects.filter(parent=loc, level="chw")
+            hh_geojson = json.dumps(
+                GeoCHWLocationSerializer(chw_objects, many=True).data)
+
+        serializer = serializer_class(loc, context={"request": self.request})
         context["target_data"] = serializer.data
 
-        response = CHWLocationViewSet.as_view({
-            "get": "retrieve"
-        })(self.request, pk=loc.pk, format="geojson")
+        view = viewset_class.as_view({"get": "retrieve"})
+        response = view(self.request, pk=loc.pk, format="geojson")
         response.render()
-        context["not_sprayable_value"] = getattr(
-            settings, "NOT_SPRAYABLE_VALUE", "noteligible")
         context["ta_geojson"] = response.content.decode()
 
-        hhview = CHWHouseholdsViewSet.as_view({"get": "retrieve"})
-        response = hhview(
-            self.request,
-            pk=loc.pk,
-            bgeom=bgeom,
-            spray_date=spray_date,
-            format="geojson",
-        )
-        response.render()
-        context["hh_geojson"] = response.content.decode()
+        context["hh_geojson"] = hh_geojson
 
+        context["not_sprayable_value"] = getattr(
+            settings, "NOT_SPRAYABLE_VALUE", "noteligible")
         context["not_sprayed_reasons"] = json.dumps(
             settings.MSPRAY_UNSPRAYED_REASON_OTHER)
+        context["spray_date"] = spray_date
 
         return context
 
