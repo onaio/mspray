e211b60710ce4a22221ebf0b53f7784f7fd24254
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-27 19:48:56 +0300

    Use `COALESCE` to ensure we dont get None values from `SUM`

--- mspray/apps/reactive/irs/queries.py
+++ mspray/apps/reactive/irs/queries.py
@@ -7,13 +7,13 @@ from mspray.apps.main.utils import parse_spray_date
 
 SPRAY_AREA_INDICATOR_SQL = """
 SELECT
-SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END) AS "other",
-SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END) AS "not_sprayable",
-SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END) AS "found",
-SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END) AS "sprayed",
-SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END) AS "new_structures",
-SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END) AS "not_sprayed",
-SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS "refused" FROM
+COALESCE(SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END),0) AS "other",
+COALESCE(SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayable",
+COALESCE(SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END),0) AS "found",
+COALESCE(SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END),0) AS "sprayed",
+COALESCE(SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END),0) AS "new_structures",
+COALESCE(SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayed",
+COALESCE(SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END),0) AS "refused" FROM
 (
   SELECT
   SUM(CASE WHEN (("main_sprayday"."data" @> '{"osmstructure:notsprayed_reASon": "refused"}' OR "main_sprayday"."data" @> '{"newstructure/gps_osm_file:notsprayed_reason": "refused"}') AND "main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = false) THEN 0 WHEN ("main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = true) THEN 0 ELSE 1 END) AS "other",
@@ -29,13 +29,13 @@ SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS "refused" FROM
 
 SPRAY_AREA_INDICATOR_SQL_DATE_FILTERED = """
 SELECT
-SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END) AS "other",
-SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END) AS "not_sprayable",
-SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END) AS "found",
-SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END) AS "sprayed",
-SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END) AS "new_structures",
-SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END) AS "not_sprayed",
-SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS "refused" FROM
+COALESCE(SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END),0) AS "other",
+COALESCE(SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayable",
+COALESCE(SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END),0) AS "found",
+COALESCE(SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END),0) AS "sprayed",
+COALESCE(SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END),0) AS "new_structures",
+COALESCE(SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayed",
+COALESCE(SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END),0) AS "refused" FROM
 (
   SELECT
   SUM(CASE WHEN (("main_sprayday"."data" @> '{"osmstructure:notsprayed_reASon": "refused"}' OR "main_sprayday"."data" @> '{"newstructure/gps_osm_file:notsprayed_reason": "refused"}') AND "main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = false) THEN 0 WHEN ("main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = true) THEN 0 ELSE 1 END) AS "other",
@@ -51,13 +51,13 @@ SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS "refused" FROM
 
 SPRAY_AREA_INDICATOR_SQL_WEEK = """
 SELECT
-SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END) AS "other",
-SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END) AS "not_sprayable",
-SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END) AS "found",
-SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END) AS "sprayed",
-SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END) AS "new_structures",
-SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END) AS "not_sprayed",
-SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS "refused" FROM
+COALESCE(SUM(CASE WHEN "other" > 0 THEN 1 ELSE 0 END),0) AS "other",
+COALESCE(SUM(CASE WHEN "not_sprayable" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayable",
+COALESCE(SUM(CASE WHEN "found" > 0 THEN 1 ELSE 0 END),0) AS "found",
+COALESCE(SUM(CASE WHEN "sprayed" > 0 THEN 1 ELSE 0 END),0) AS "sprayed",
+COALESCE(SUM(CASE WHEN "new_structures" > 0 THEN 1 ELSE 0 END),0) AS "new_structures",
+COALESCE(SUM(CASE WHEN "not_sprayed" > 0 THEN 1 ELSE 0 END),0) AS "not_sprayed",
+COALESCE(SUM(CASE WHEN "refused" >0 THEN 1 ELSE 0 END) AS,0) "refused" FROM
 (
   SELECT
   SUM(CASE WHEN (("main_sprayday"."data" @> '{"osmstructure:notsprayed_reASon": "refused"}' OR "main_sprayday"."data" @> '{"newstructure/gps_osm_file:notsprayed_reason": "refused"}') AND "main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = false) THEN 0 WHEN ("main_sprayday"."sprayable" = true AND "main_spraypoint"."id" IS NOT NULL AND "main_sprayday"."was_sprayed" = true) THEN 0 ELSE 1 END) AS "other",
