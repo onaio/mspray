a4e152618b34105a96f48c659c94a6b2e61a2c94
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-13 16:07:18 +0300

    Use the new reactive irs home view

--- mspray/apps/reactive/irs/urls.py
+++ mspray/apps/reactive/irs/urls.py
@@ -11,8 +11,8 @@ from mspray.apps.main.views import (
     sprayday,
     target_area,
 )
-from mspray.apps.reactive.irs.views import (CHWDistrictsView, CHWListView,
-                                            CHWLocationMapView)
+from mspray.apps.reactive.irs.views import (CHWListView, CHWLocationMapView,
+                                            HomeView)
 
 router = routers.DefaultRouter(trailing_slash=False)  # pylint: disable=C0103
 
@@ -22,10 +22,11 @@ router.register(r"households", household.HouseholdViewSet)
 router.register(r"spraydays", sprayday.SprayDayViewSet)
 router.register(r"targetareas", target_area.TargetAreaViewSet)
 
+
 # pylint: disable=invalid-name
 app_name = "reactive_irs"
 urlpatterns = [
-    path("", LocationCHWView.as_view(), name="location_chw_list"),
+    path("", HomeView.as_view(), name="reactive_irs_home"),
     path("<int:pk>", CHWListView.as_view(), name="chw_list"),
     path("map/<int:pk>", CHWLocationMapView.as_view(), name="chw_list_map"),
     path("chw/map/<int:pk>", CHWLocationMapView.as_view(), name="chw_map"),
